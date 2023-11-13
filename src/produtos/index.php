dd90f80eb2e711958afe814482ba32e956339ed3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-14 00:26:36 +0200

    atualizações

--- src/produtos/index.php
+++ src/produtos/index.php
@@ -182,7 +182,7 @@
                 ?>
                 <div class="card mb-3 p-3">
                     <div class="row">
-                      <div class="col-12">
+                      <div class="col-12 align-content-end">
                         <div class="form-check form-switch">
                           <input class="form-check-input situacao" type="checkbox" <?=(($d->codigo == 1)?'disabled':false)?> <?=(($d->situacao)?'checked':false)?> situacao="<?=$d->codigo?>">
                           Situação
