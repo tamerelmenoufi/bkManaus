93791ac7131e98a24a2ee1a2f58a7eb356f501b8
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-13 21:09:49 +0200

    atualizações

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -30,6 +30,14 @@
       </div>
     </div>
 
+    <div class="row mb-1">
+      <div class="col">
+        <a url="src/categorias/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-regular fa-user col-1"></i> <span class="col-11">Categorias de Produtos</span>
+        </a>
+      </div>
+    </div>
+
   </div>
 </div>
 
