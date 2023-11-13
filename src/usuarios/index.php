cc7689724b052f861d3636ff2a6d07a37fc1ef55
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-13 20:20:43 +0200

    atualizações

--- src/usuarios/index.php
+++ src/usuarios/index.php
@@ -46,19 +46,7 @@
                 <div class="input-group mb-3">
                   <label class="input-group-text" for="inputGroupFile01">Buscar por </label>
                   <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" rotulo_busca aria-expanded="false"><?=((!$_SESSION['usuarioBuscaCampo'] or $_SESSION['usuarioBuscaCampo'] == 'nome')?'Nome':(($_SESSION['usuarioBuscaCampo'] == 'perfil')?'Perfil':(($_SESSION['usuarioBuscaCampo'] == 'pac')?'PAC':'CPF')))?></button>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#" opcao_busca="Nome">Nome</a></li>
-                    <li><a class="dropdown-item" href="#" opcao_busca="CPF">CPF</a></li>
-                    <li><a class="dropdown-item" href="#" opcao_busca="Perfil">Perfil</a></li>
-                  </ul>
                   <input type="text" texto_busca style="display:<?=(($_SESSION['usuarioBuscaCampo'] == 'perfil' or $_SESSION['usuarioBuscaCampo'] == 'pac')?'none':'block')?>" class="form-control" value="<?=$_SESSION['usuarioBusca']?>" aria-label="Digite a informação para a busca">
-                  <select busca_perfil class="form-control" style="display:<?=(($_SESSION['usuarioBuscaCampo'] != 'perfil')?'none':'block')?>">
-                    <option value="adm" <?=(($_SESSION['usuarioBusca'] == 'adm')?'selected':false)?>>Administrador</option>
-                    <option value="sup" <?=(($_SESSION['usuarioBusca'] == 'sup')?'selected':false)?>>Supervisor</option>
-                    <option value="crd" <?=(($_SESSION['usuarioBusca'] == 'crd')?'selected':false)?>>Coordenador</option>
-                    <option value="usr" <?=(($_SESSION['usuarioBusca'] == 'usr')?'selected':false)?>>Agente</option>
-                  </select>
-
                   <button filtrar class="btn btn-outline-secondary" type="button">Buscar</button>
                   <button limpar class="btn btn-outline-danger" type="button">limpar</button>
                 </div>
