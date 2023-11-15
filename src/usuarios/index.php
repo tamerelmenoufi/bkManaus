2f490582a0252fab63ec731dc2dc0d3ddebc5867
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-15 16:48:27 +0200

    atualizações

--- src/usuarios/index.php
+++ src/usuarios/index.php
@@ -36,6 +36,13 @@
     background-color:#a1a1a1;
     cursor: pointer;
   }
+  td, th{
+    white-space: nowrap;
+  }
+  .card label{
+    font-size:10px;
+    color:#a1a1a1;
+  }
 </style>
 <div class="col">
   <div class="m-3">
@@ -45,24 +52,56 @@
         <div class="card">
           <h5 class="card-header">Lista de Usuários</h5>
           <div class="card-body">
-            <div class="d-flex justify-content-between">
-                <div class="input-group mb-3">
-                  <label class="input-group-text" for="inputGroupFile01">Buscar por </label>
-                  <input campoBusca type="text" class="form-control" value="<?=$_SESSION['usuarioBusca']?>" aria-label="Digite a informação para a busca">
-                  <button filtro="filtrar" class="btn btn-outline-secondary" type="button">Buscar</button>
-                  <button filtro="limpar" class="btn btn-outline-danger" type="button">limpar</button>
-                </div>
+            <div class="d-none d-md-block">
+              <div class="d-flex justify-content-between">
+                  <div class="input-group mb-3">
+                    <label class="input-group-text" for="inputGroupFile01">Buscar por </label>
+                    <input campoBusca type="text" class="form-control" value="<?=$_SESSION['usuarioBusca']?>" aria-label="Digite a informação para a busca">
+                    <button filtro="filtrar" class="btn btn-outline-secondary" type="button">Buscar</button>
+                    <button filtro="limpar" class="btn btn-outline-danger" type="button">limpar</button>
+                  </div>
 
 
-                <button
-                    novoCadastro
-                    class="btn btn-success btn-sm"
-                    data-bs-toggle="offcanvas"
-                    href="#offcanvasDireita"
-                    role="button"
-                    aria-controls="offcanvasDireita"
-                    style="margin-left:20px;"
-                >Novo</button>
+                  <button
+                      novoCadastro
+                      class="btn btn-success btn-sm"
+                      data-bs-toggle="offcanvas"
+                      href="#offcanvasDireita"
+                      role="button"
+                      aria-controls="offcanvasDireita"
+                      style="margin-left:20px;"
+                  >Novo</button>
+              </div>
+            </div>
+
+            <div class="d-block d-md-none d-lg-none d-xl-none d-xxl-none">
+              <div class="d-flex justify-content-between mb-3">
+
+                  <div class="row">
+                    <div class="col-12 mb-2">
+                      <div class="input-group">
+                        <label class="input-group-text" for="inputGroupFile01">Buscar por </label>
+                        <input campoBusca type="text" class="form-control" value="<?=$_SESSION['usuarioBusca']?>" aria-label="Digite a informação para a busca">
+                      </div>
+                    </div>
+                    <div class="col-12 mb-2">
+                      <button filtro="filtrar" class="btn btn-outline-secondary w-100" type="button">Buscar</button>
+                    </div>
+                    <div class="col-12 mb-2">
+                      <button filtro="limpar" class="btn btn-outline-danger w-100" type="button">limpar</button>
+                    </div>
+                    <div class="col-12 mb-2">
+                      <button
+                          novoCadastro
+                          class="btn btn-success btn-sm w-100"
+                          data-bs-toggle="offcanvas"
+                          href="#offcanvasDireita"
+                          role="button"
+                          aria-controls="offcanvasDireita"
+                      >Novo</button>                      
+                    </div>
+                  </div>
+              </div>
             </div>
 
 
