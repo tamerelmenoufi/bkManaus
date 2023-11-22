1f628cc382306ef8e4454ad6f2a5d04b1f911ee7
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-22 21:09:28 +0200

    atualizações

--- src/produtos/form.php
+++ src/produtos/form.php
@@ -95,7 +95,7 @@
         <div class="row">
             <div class="col">
                 <?php
-echo $dados;
+echo "Itens: ".$dados;
                 ?>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="produto" name="produto" placeholder="Nome do produto" value="<?=$d->produto?>">
