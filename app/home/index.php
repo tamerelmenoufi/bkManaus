1279dd022722e7c9f6a1bc00a1c70cc0459e30a8
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-27 23:31:52 +0200

    atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -28,7 +28,7 @@ $(function(){
         slidesToScroll: 1,
         arrows: false,
         fade: true,
-        asNavFor: '.slider-nav'
+        // asNavFor: '.slider-nav'
     });
     // $('.slider-nav').slick({
     //     slidesToShow: 3,
