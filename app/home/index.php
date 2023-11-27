37b08faf4f673ad2d68c59847e2f5968cc49a9b3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-27 23:29:30 +0200

    atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -1,1 +1,46 @@
 Página principal do app
+<style>
+    .slider-for div{
+        width:200px;
+        height:200px;
+        background:#ccc;
+        color:#000;
+        text-align:center;
+    }
+</style>
+
+
+<div class="slider-for">
+  <div><h3>1</h3></div>
+  <div><h3>2</h3></div>
+  <div><h3>3</h3></div>
+  <div><h3>4</h3></div>
+  <div><h3>5</h3></div>
+  <div><h3>6</h3></div>
+</div>
+			
+<script>
+
+$(function(){
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    // $('.slider-nav').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.slider-for',
+    //     dots: true,
+    //     centerMode: true,
+    //     focusOnSelect: true
+    // });
+
+})
+
+	
+
+</script>
