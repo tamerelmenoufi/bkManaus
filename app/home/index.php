aab25aa023326c5fdc5f0eccc7708c96f76b74d5
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-28 00:05:26 +0200

    atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -4,9 +4,9 @@
 <style>
     .slider-for{
         position:absolute;
+        top:0;
+        bottom:0;
         width:100%;
-        height:100%;
-        clear:both;
     }
     .slider-for div{
         margin:0;
