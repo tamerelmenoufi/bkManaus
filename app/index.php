9eb266f9f222deafcad5308e6ce70bf7bfc2dae6
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-27 23:06:53 +0200

    atualizações

---
+++ app/index.php
@@ -0,0 +1,49 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="img/icone.png">
+    <title>BK - Manaus</title>
+    <?php
+    include("../lib/header.php");
+    ?>
+  </head>
+  <body translate="no">
+
+    <div class="Carregando">
+        <div><i class="fa-solid fa-rotate fa-pulse"></i></div>
+    </div>
+
+    <div class="CorpoApp"></div>
+
+    <?php
+    include("../lib/footer.php");
+    ?>
+
+    <script>
+        $(function(){
+            // Carregando();
+            $.ajax({
+                url:"home/index.php",
+                success:function(dados){
+                    $(".CorpoApp").html(dados);
+                }
+            });
+        })
+
+
+        //Jconfirm
+        jconfirm.defaults = {
+            typeAnimated: true,
+            type: "blue",
+            smoothContent: true,
+        }
+
+    </script>
+
+  </body>
+</html>
