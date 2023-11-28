687eba8756c33bfab8a026eabb6ad89ad34246d0
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-28 07:43:29 +0200

    atualizações

--- app/rodape/rodape.php
+++ app/rodape/rodape.php
@@ -29,6 +29,6 @@ include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 </style>
 <div class="rodape">
     <div class="rodape_area">
-        <img src="img/logo.csv" />
+        <img src="img/logo.svg" />
     </div>
 </div>
