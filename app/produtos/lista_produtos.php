5d2d1b9c2a2b11db8fb4d05a1982ecb871942e90
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-28 17:55:24 +0200

    atualizações

--- app/produtos/lista_produtos.php
+++ app/produtos/lista_produtos.php
@@ -1,3 +1,44 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 ?>
+<style>
+    .home_corpo{
+        position: absolute;
+        top:0;
+        bottom:90px;
+        overflow:auto;
+        background-color:#fff;
+        width:100%;
+    }
+
+    .home_rodape{
+        position: absolute;
+        background-color:#fff;
+        width:100%;
+        bottom:0;
+        height:90px;
+    }
+</style>
+
+<div class="home_corpo">
+
+</div>
+<div class="home_rodape"></div>
+
+<script>
+
+$(function(){
+
+    $.ajax({
+        url:"home/banner.php",
+        success:function(dados){
+            $(".home_promocao").html(dados);
+        }
+    });
+
+
+})
+
+	
+
+</script>
