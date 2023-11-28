b26070fd73a5496eb24eafb30228104b7ebc508b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-28 09:18:29 +0200

    atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -2,33 +2,9 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 ?>
 <style>
-    .slider{
-        position:absolute;
-        background:#fff;
-        top:0;
-        bottom:0;
-        width:100%;        
+    .home_promocao{
+
     }
-    .slider-for{
-        position:relative;
-        width:100%;
-    }
-    .slider-for img{
-        margin:0;
-        padding:0;
-        width:100%;
-        height:auto;
-    }
-    .barra_banner{
-        position: absolute;
-        margin-top:-30px;
-        height:30px;
-        width:100%;
-    }
-    .barra_banner div{
-        background-color:#fff;
-        width:100%;
-    } 
     .home_rodape{
         position: relative;
         background-color:#fff;
@@ -38,27 +14,19 @@
     }
 </style>
 
-<div class="slider">
-    <div class="slider-for">
-        <img src="img/banner.png" />
-        <img src="img/banner.png" />
-        <img src="img/banner.png" />
-        <img src="img/banner.png" />
-    <!-- <div style="background-image:url(img/banner.png); background-size:contain;"></div> -->
-    </div>
-    <div class="d-flex justify-content-center barra_banner">
-        <div></div>
-        <img src="img/banner_seta.png" />
-        <div></div>
-    </div>
-    <div class="home_rodape"></div>
-</div>
-
-
+<div class="home_promocao"></div>
+<div class="home_rodape"></div>
 
 <script>
 
 $(function(){
+
+    $.ajax({
+        url:"rodape/banner.php",
+        success:function(dados){
+            $(".home_promocao").html(dados);
+        }
+    });
 
     $.ajax({
         url:"rodape/rodape.php",
@@ -67,14 +35,7 @@ $(function(){
         }
     });
 
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-    });
+
 
 })
 
