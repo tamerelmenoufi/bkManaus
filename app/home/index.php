9765140d48e054f23a77dcb8a9fc5ac8c4815519
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-28 09:45:09 +0200

    atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -13,26 +13,24 @@
     }
 
 
-    /* Works on Firefox */
-* {
-  scrollbar-width: thin;
-  scrollbar-color: blue orange;
-}
+    * {
+        scrollbar-width: thin;
+        scrollbar-color: black transparent;
+    }
 
-/* Works on Chrome, Edge, and Safari */
-*::-webkit-scrollbar {
-  width: 12px;
-}
+    *::-webkit-scrollbar {
+        width: 3px;
+        height: 3px; /* A altura só é vista quando a rolagem é horizontal */
+    }
 
-*::-webkit-scrollbar-track {
-  background: orange;
-}
+    *::-webkit-scrollbar-track {
+        background: transparent;
+        padding: 2px;
+    }
 
-*::-webkit-scrollbar-thumb {
-  background-color: blue;
-  border-radius: 20px;
-  border: 3px solid orange;
-}
+    *::-webkit-scrollbar-thumb {
+        background-color: #000;
+    }
 
 
 
