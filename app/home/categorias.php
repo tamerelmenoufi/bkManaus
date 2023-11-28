31415347e78ae04721ec3f63464db2736b9ae49a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-28 10:04:02 +0200

    atualizações

--- app/home/categorias.php
+++ app/home/categorias.php
@@ -1,7 +1,30 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 ?>
-
-<div class="">
-    Categorias neste espaço
+<style>
+    .combo{
+        margin:5px;
+        background-color:red;
+        border-radius:20px;
+    }
+    .categorias{
+        margin:5px;
+        background-color:orange;
+        border-radius:20px;
+    }
+    
+</style>
+<div class="row g-0">
+    <div class="col-12">
+        <div class="combo"></div>
+    </div>
+    <?php
+    for($i=0;$i<6;$i++){
+    ?>
+    <div class="col-6">
+        <div class="categorias"></div>
+    </div>
+    <?php
+    }
+    ?>
 </div>
