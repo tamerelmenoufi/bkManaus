3c5265c122f11e308fccc2a0fc9a7af23ac1c201
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-28 22:22:49 +0200

    atualizações

--- app/produtos/lista_produtos.php
+++ app/produtos/lista_produtos.php
@@ -93,7 +93,7 @@ while($d = mysqli_fetch_object($result)){
                 <h4 style="color:#f12a2a"><?=$d->produto?></h4>
             </div>
             <div class="produto_dados" style="height:60px;">
-                <div style="color:#c45018; font-family:FlameBold; font-size:20px; display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 2;"><?=$d->descricao?></div>
+                <div style="color:#c45018; overflow: hidden; font-family:FlameBold; font-size:20px; display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 2;"><?=$d->descricao?></div>
             </div>
             <div class="produto_dados">
                 <h2 style="color:#f12a2a">R$ <?=number_format($d->valor,2,",",false)?></h2>
