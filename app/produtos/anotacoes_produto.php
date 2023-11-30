a28e985a00affeaadc90a495bcb5826d19b304d7
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-11-30 18:43:41 +0200

    atualizações

--- app/produtos/anotacoes_produto.php
+++ app/produtos/anotacoes_produto.php
@@ -189,24 +189,10 @@
                 $r = mysqli_query($con, $q);
                 while($i = mysqli_fetch_object($r)){
                 ?>
-                <li class="list-group-item d-flex justify-content-between flex-column">
+                <li class="list-group-item">
                     <div class="form-check">
                         <input type="checkbox" class="form-check-input" id="remocao<?=$i->codigo?>">
                         <label class="form-check-label" for="remocao<?=$i->codigo?>"><?=$i->item?></label>
-                    </div>
-                    <div class="d-flex justify-content-end w-100">
-                        <div class="input-group" style="width:160px;">
-                            <select class="form-select form-select-sm" id="remocao_valor<?=$i->codigo?>">
-                                <?php
-                                for($j=1;$j<=10;$j++){
-                                ?>
-                                <option value="<?=$j?>"><?=$j?></option>
-                                <?php
-                                }
-                                ?>
-                            </select>
-                            <label class="input-group-text" style="width:100px; text-align:right;" for="remocao_valor<?=$i->codigo?>">R$ <?=number_format($i->valor, 2, ",", false)?></label>
-                        </div>                        
                     </div>
                 </li>
                 <?php
