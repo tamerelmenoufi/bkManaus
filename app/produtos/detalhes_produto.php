65fc9bfd6bab8d31e53e7700eb4d70d0fce16069
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-02 02:21:31 +0200

    atualizações

--- app/produtos/detalhes_produto.php
+++ app/produtos/detalhes_produto.php
@@ -43,8 +43,8 @@
     if($_POST['acao'] == 'salvar'){
 
         echo $q = "update vendas_tmp set detalhes = JSON_SET(detalhes, 
-                                                detalhes->'$.item{$_POST['codigo']}.quantidade', '{$_POST['quantidade']}',
-                                                detalhes->'$.item{$_POST['codigo']}.status' , 'true')
+                                                '$.item{$_POST['codigo']}.quantidade', '{$_POST['quantidade']}',
+                                                '$.item{$_POST['codigo']}.status' , 'true')
                             where id_unico = '{$_POST['idUnico']}'";
 
         mysqli_query($con, $q);
