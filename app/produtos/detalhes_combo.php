4f7d20f3518ea3f7a656074099fe117c36a113a7
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-03 20:24:14 +0200

    atualizações

--- app/produtos/detalhes_combo.php
+++ app/produtos/detalhes_combo.php
@@ -478,8 +478,8 @@ $(function(){
             acao = $(this).prop("checked")
             if(!combo.produtos) {combo.produtos = [];}
             if(!combo.produtos[produto]) {combo.produtos[produto] = produto;}
-            if(!combo.produtos[produto].codigo) combo.produtos.codigo = produto;
-            if(!combo.produtos[produto].remocao) combo.produtos.remocao = [];
+            if(!combo.produtos[produto].codigo) combo.produtos[produto].codigo = produto;
+            if(!combo.produtos[produto].remocao) combo.produtos[produto].remocao = [];
             if(acao == true){
                 combo.produtos[produto].remocao.push(codigo);
             }
