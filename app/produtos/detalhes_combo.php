1d6e706d8c42fea6add500a863d12d634b2da85d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-03 10:56:05 +0200

    atualizações

--- app/produtos/detalhes_combo.php
+++ app/produtos/detalhes_combo.php
@@ -469,7 +469,7 @@ $(function(){
             console.log("Produto:" + produto)
             console.log("Codigo:" + codigo)
             if($(this).prop("checked") == true){
-                remocao[produto][codigo] = codigo
+                remocao[(produto*1)][(codigo*1)] = codigo
             }
         })
 
