b9ce45de641e48798be3a99214b0827620c52609
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-03 09:34:05 +0200

    atualizações

--- app/produtos/detalhes_produto.php
+++ app/produtos/detalhes_produto.php
@@ -299,7 +299,7 @@
                 ?>
                 <li class="list-group-item d-flex justify-content-between">
                     <div class="form-check">
-                        <input type="checkbox" <?=(($remocao[$i->codigo] == $i->codigo)?'checked':false)?> class="form-check-input substituicao" name="substituicao" codigo="<?=$i->codigo?>" valor="<?=$i->valor?>" id="substituicao<?=$i->codigo?>">
+                        <input type="checkbox" <?=(($substituicao[$i->codigo] == $i->codigo)?'checked':false)?> class="form-check-input substituicao" name="substituicao" codigo="<?=$i->codigo?>" valor="<?=$i->valor?>" id="substituicao<?=$i->codigo?>">
                         <label class="form-check-label" for="substituicao<?=$i->codigo?>"><?=$i->item?></label>
                     </div>
                     <div>
