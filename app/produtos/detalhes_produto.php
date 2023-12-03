ff44e6c5a7d9a5ffbfe587c1e30e75fa524863db
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-03 07:44:16 +0200

    atualizações

--- app/produtos/detalhes_produto.php
+++ app/produtos/detalhes_produto.php
@@ -474,7 +474,8 @@ $(function(){
             success:function(dados){
                 valor = (dados*1);
                 $(".adicionar").html('R$ ' + (valor*qt).toLocaleString('pt-br', {minimumFractionDigits: 2}));  
-                $(".CorpoApp").html(valor);
+                $(".adicionar").valor("valor", valor);  
+                // $(".CorpoApp").html(valor);
                 // Carregando('none');
             }
         });        
