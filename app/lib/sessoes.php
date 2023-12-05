4492a2388ad4fcd9c5de03867434cd4f8037c437
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-05 13:11:40 +0200

    atualizações

--- app/lib/sessoes.php
+++ app/lib/sessoes.php
@@ -3,5 +3,10 @@
 
     print_r($_SESSION['historico']);
 
-    
+    $i = ((count($_SESSION['historico']))?(count($_SESSION['historico']) -1):0);
+    $r['pg'] = $_SESSION['historico'][$i]['local'];
+    $r['tg'] = $_SESSION['historico'][$i]['destino'];
+
+    echo json_encode($r);
+
 ?>
