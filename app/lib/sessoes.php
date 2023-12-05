e7818e9ff632c8ab9ca2ddf8d0eb5442be1c8c20
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-05 13:20:11 +0200

    atualizações

--- app/lib/sessoes.php
+++ app/lib/sessoes.php
@@ -1,12 +1,3 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
-
-    print_r($_SESSION['historico']);
-
-    $i = ((count($_SESSION['historico']))?(count($_SESSION['historico']) -1):0);
-    $r['pg'] = $_SESSION['historico'][$i]['local'];
-    $r['tg'] = $_SESSION['historico'][$i]['destino'];
-
-    echo json_encode($r);
-
 ?>
---
+++ app/lib/voltar.php
@@ -0,0 +1,10 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+    $i = ((count($_SESSION['historico']))?(count($_SESSION['historico']) -1):0);
+    $r['pg'] = $_SESSION['historico'][$i]['local'];
+    $r['tg'] = $_SESSION['historico'][$i]['destino'];
+
+    echo json_encode($r);
+
+?>
