b61712f509b26986a44994970927843cf99be827
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-05 12:52:12 +0200

    atualizações

---
+++ app/lib/sessoes.php
@@ -0,0 +1,5 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+    print_r($_SESSION['historico']);
+?>
--- lib/includes.php
+++ lib/includes.php
@@ -7,3 +7,13 @@
     $md5 = md5(date("YmdHis"));
 
     $urlPainel = 'https://painel.bkmanaus.com.br/';
+
+    if($_POST['historico']){
+        $pagina = $_SERVER["PHP_SELF"];
+        $destino = $_POST['historico'];
+        $i = ((count($_SESSION['historico']))?(count($_SESSION['historico']) -1):0);
+        if($_SESSION['historico'][$i] != $pagina){
+            $_SESSION['historico'][] = $pagina;
+        } 
+        unset($_POST['historico']);
+    }
