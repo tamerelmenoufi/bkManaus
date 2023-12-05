e87abf61a8744d6a13649da898d3bc20801c57f5
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-05 13:33:29 +0200

    atualizações

--- app/lib/voltar.php
+++ app/lib/voltar.php
@@ -1,7 +1,8 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 
-    $i = ((count($_SESSION['historico']))?(count($_SESSION['historico']) -1):0);
+    $i = ((count($_SESSION['historico']))?(count($_SESSION['historico']) -2):0);
+    unset($_SESSION['historico'][($i+1)]);
     $r['pg'] = $_SESSION['historico'][$i]['local'];
     $r['tg'] = $_SESSION['historico'][$i]['destino'];
 
