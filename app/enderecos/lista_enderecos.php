160f9073167c78ebc50b2dd1350fc3faa89cd93e
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-06 21:00:37 +0200

    atualizações

--- app/enderecos/lista_enderecos.php
+++ app/enderecos/lista_enderecos.php
@@ -8,7 +8,19 @@
         $_SESSION['codUsr'] = $_POST['codUsr'];
     }
 
-    
+
 ?>
 
 Meus endereços
+
+<div class="row g-0">
+    <div class="card m-2">
+        <div class="input-group mb-3">
+            <label for="cep" class="form-label">Cadstro de endereço</label>
+            <input type="text" class="form-control" id="cep" inputmode="numeric" placeholder="XXXXX-XXX" aria-label="Digite o CEP" aria-describedby="cadastro_cep">
+            <button class="btn btn-outline-secondary cep" type="button" id="cadastro_cep">Avançar</button>
+        </div>
+        <div id="emailHelp" class="form-text">Avançar com o CEP preenchido agiliza o cadastro do seu endereço</div>
+  </div>
+    </div>
+</div>
