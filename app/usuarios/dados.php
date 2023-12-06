3d7ff26b71995edf874786190aad3f1547a899c1
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-06 19:57:10 +0200

    atualizações

--- app/usuarios/dados.php
+++ app/usuarios/dados.php
@@ -10,6 +10,17 @@
         $where = " where codigo = '{$_SESSION['codUsr']}'";
     }
 
+    if($_SPOT['acao'] == 'atualizar'){
+        mysqli_query($con, "update clientes set {$_POST['campo']} = '{$_POST['valor']}'");
+        $retorno = [
+            'status' => 'success',
+            'idUnico' => $_SESSION['idUnico'],
+            'codUsr' => $_SESSION['codUsr'],
+        ];
+        echo json_decode($retorno);
+        exit();
+    }
+
     if($_POST['telefone']){
         $q = "SELECT * from clientes WHERE telefone = '{$_POST['telefone']}'";
         $c = mysqli_fetch_object(mysqli_query($con, $q));
@@ -30,12 +41,65 @@
 
 ?>
 
+<div class="row g-0 p-3">
+    <div class="col">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome Completo</label>
+            <input type="text" class="form-control formDados" id="nome">
+        </div>
+        <div class="mb-3">
+            <label for="cpf" class="form-label">CPF</label>
+            <input type="text" class="form-control formDados" id="cpf">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Telefone</label>
+            <div class="form-control is-valid" ><?=$d->telefone?></div>
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">E-mail</label>
+            <input type="email" class="form-control formDados" id="email">
+        </div>        
+    </div>
+</div>
+
+
 <script>
     $(function(){
 
         localStorage.setItem("codUsr", '<?=$_SESSION['codUsr']?>');
 
-        
+        ExecutaAtualizacao = (campo, valor)=>{
+            $.ajax({
+                url:"usuarios/dados.php",
+                type:"POST",
+                dataType:"JSON",
+                data:{
+                    campo,
+                    valor,
+                    acao:'atualizar'
+                },
+                success:function(dados){
+                    console.log(dados)
+                }
+            })            
+        }
+
+        $(".formDados").change(function(){
+            campo = $(this).attr("id");
+            valor = $(this).val();
+            if(campo == 'nome'){
+                ExecutaAtualizacao(campo, valor);
+            }else if(campo == 'cpf'){
+                if(valor.length == 14){
+                    ExecutaAtualizacao(campo, valor);
+                }
+            }else if(campo == 'email'){
+                ExecutaAtualizacao(campo, valor);
+            }
+        })
+
+
+
 
 
     })
