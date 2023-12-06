ca5cd0246d1bf26016faccb91a8108f738e5264a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-06 20:29:47 +0200

    atualizações

--- app/usuarios/dados.php
+++ app/usuarios/dados.php
@@ -47,15 +47,15 @@
             <label for="nome" class="form-label">Nome Completo</label>
             <input type="text" class="form-control formDados" autocomplete="off" value="<?=$d->nome?>" id="nome">
         </div>
-        <div class="mb-13">
+        <div class="mb-1">
             <label for="cpf" class="form-label">CPF</label>
             <input type="text" class="form-control formDados" autocomplete="off" value="<?=$d->cpf?>" id="cpf">
         </div>
-        <div class="mb-13">
+        <div class="mb-1">
             <label class="form-label">Telefone</label>
             <div class="form-control is-valid" ><?=$d->telefone?></div>
         </div>
-        <div class="mb-1">
+        <div>
             <label for="email" class="form-label">E-mail</label>
             <input type="email" class="form-control formDados" autocomplete="off" value="<?=$d->email?>" id="email">
         </div>        
