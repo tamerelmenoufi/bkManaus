ce79d01dc8435feef25fa7bb041c4d643158fe3e
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-06 15:43:24 +0200

    atualizações

--- app/rodape/rodape.php
+++ app/rodape/rodape.php
@@ -14,8 +14,8 @@ include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
     .rodape_area{
         position:absolute;
         margin:10px;
-        margin-top:15px;
-        margin-bottom:15px;
+        margin-top:20px;
+        margin-bottom:20px;
         padding-right:30px;
         border-radius:40px;
         background-color:#b60710;
