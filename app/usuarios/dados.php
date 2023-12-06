34f4afde149e107138d84cf256704df7c200e148
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-06 20:28:02 +0200

    atualizações

--- app/usuarios/dados.php
+++ app/usuarios/dados.php
@@ -41,21 +41,21 @@
 
 ?>
 
-<div class="row g-0 p-2">
+<div class="row g-0 p-3">
     <div class="card p-1">
-        <div class="mb-3">
+        <div class="mb-1">
             <label for="nome" class="form-label">Nome Completo</label>
             <input type="text" class="form-control formDados" autocomplete="off" value="<?=$d->nome?>" id="nome">
         </div>
-        <div class="mb-3">
+        <div class="mb-13">
             <label for="cpf" class="form-label">CPF</label>
             <input type="text" class="form-control formDados" autocomplete="off" value="<?=$d->cpf?>" id="cpf">
         </div>
-        <div class="mb-3">
+        <div class="mb-13">
             <label class="form-label">Telefone</label>
             <div class="form-control is-valid" ><?=$d->telefone?></div>
         </div>
-        <div class="mb-3">
+        <div class="mb-1">
             <label for="email" class="form-label">E-mail</label>
             <input type="email" class="form-control formDados" autocomplete="off" value="<?=$d->email?>" id="email">
         </div>        
