2811d6902613b3f9686f6f38ca0b24ab9ed984ba
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-06 21:03:14 +0200

    atualizações

--- app/enderecos/lista_enderecos.php
+++ app/enderecos/lista_enderecos.php
@@ -11,11 +11,11 @@
 
 ?>
 
-<div class="row g-0 p-2">
+<div class="row g-0 p-2 mt-3">
     <div class="card p-2">
         <h4 class="w-100 text-center">Endereços</h4>
-        <div class="input-group mt-3">
-            <label for="cep" class="form-label">Cadstro de endereço</label>
+        <label for="cep" class="form-label">Cadstro de endereço</label>
+        <div class="input-group">
             <input type="text" class="form-control" id="cep" inputmode="numeric" placeholder="XXXXX-XXX" aria-label="Digite o CEP" aria-describedby="cadastro_cep">
             <button class="btn btn-outline-secondary cep" type="button" id="cadastro_cep">Avançar</button>
         </div>
