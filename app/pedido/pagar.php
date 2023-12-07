8584dc831f03c25e278904f0a57918dfb8f4c130
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-07 23:22:48 +0200

    atualizações

--- app/pedido/pagar.php
+++ app/pedido/pagar.php
@@ -92,7 +92,7 @@ $(function(){
                 codUsr
             },
             success:function(dados){
-                $(`${local}`).html(dados);
+                $(`.${local}`).html(dados);
             }
         });        
     }
