1d0ba4dc03c8d736f437247622cd202ea623b659
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-07 23:18:12 +0200

    atualizações

---
+++ app/pedido/pagar.php
@@ -0,0 +1,108 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+    if($_POST['idUnico']){
+        $_SESSION['idUnico'] = $_POST['idUnico'];
+    }
+
+    if($_POST['codUsr']){
+        $_SESSION['codUsr'] = $_POST['codUsr'];
+    }
+?>
+
+<style>
+    .barra_topo{
+        position:absolute;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        flex-direction: column;
+        top:0;
+        width:100%;
+        height:100px;
+        background-color:#ffc63a;
+        color:#c45018;
+        border-bottom-right-radius:40px;
+        border-bottom-left-radius:40px;
+        font-family:FlameBold;
+    }
+
+
+    .home_corpo{
+        position: absolute;
+        top:100px;
+        bottom:80px;
+        overflow:auto;
+        background-color:#fff;
+        width:100%;
+    }
+
+    .home_rodape{
+        position: absolute;
+        background-color:#fff;
+        width:100%;
+        bottom:0;
+        height:80px;
+    }
+
+
+</style>
+
+<div class="barra_topo">
+    <h2>Pagar</h2>
+</div>
+
+
+<div class="home_corpo">
+    <div class="dados_pedido" local="pedido/pedido.php"></div>
+    <div class="dados_pessoais" local="pedido/usuario.php"></div>
+    <div class="dados_enderecos" local="pedido/endereco.php"></div>
+    <div class="dados_pagamento" local="pedido/pagamento.php"></div>
+</div>   
+<div class="home_rodape"></div>
+
+<script>
+
+$(function(){
+
+    $.ajax({
+        url:"rodape/rodape.php",
+        success:function(dados){
+            $(".home_rodape").html(dados);
+        }
+    });
+
+    $.ajax({
+        url:"topo/topo.php",
+        success:function(dados){
+            $(".barra_topo").append(dados);
+        }
+    });
+
+    idUnico = localStorage.getItem("idUnico");
+    codUsr = localStorage.getItem("codUsr");
+
+
+    AbrirTelas = (tela, local)=>{
+        $.ajax({
+            url:tela,
+            type:"POST",
+            data:{
+                idUnico,
+                codUsr
+            },
+            success:function(dados){
+                $(`${local}`).html(dados);
+            }
+        });        
+    }
+
+
+    
+    
+
+    //Novos testes
+
+})
+
+</script>
---
+++ app/pedido/pedido.php
@@ -0,0 +1,76 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+    if($_POST['idUnico']){
+        $_SESSION['idUnico'] = $_POST['idUnico'];
+    }
+
+    if($_POST['codUsr']){
+        $_SESSION['codUsr'] = $_POST['codUsr'];
+        $where = " where codigo = '{$_SESSION['codUsr']}'";
+    }
+
+    $query = "select * from vendas_tmp where id_unico = '{$_POST['idUnico']}'";
+
+    $result = mysqli_query($con, $query);
+
+    $d = mysqli_fetch_object($result);
+
+?>
+
+<style>
+    .enderecoLabel{
+        white-space: nowrap;
+        width: 100%;
+        overflow: hidden; /* "overflow" value must be different from "visible" */
+        text-overflow: ellipsis;
+        color:#333;
+        font-size:14px;
+        cursor:pointer;
+    }
+</style>
+
+<div class="row g-0 p-2 mt-3">
+    <div class="card p-2">
+        <h4 class="w-100 text-center">Resumo do Pedido</h4>
+        <div class="d-flex justify-content-between">
+            
+                <?php
+
+                    foreach(json_decode($d->detalhes) as $i => $dados){
+
+                        $pd = mysqli_fetch_object(mysqli_query($con, "select * from produtos where codigo = '{$dados->codigo}'"));
+                        if($dados->status){
+                ?>
+                <div class="enderecoLabel" codigo="<?=$c->codigo?>">
+                    <i class="fa-solid fa-location-dot"></i>
+                <?php
+                            echo $pd->produto." - ". $dados->total ."x".$dados->quantidade." = ".($dados->total*$dados->quantidade)."<br>";
+                            $total = ($total + ($dados->total*$dados->quantidade));
+
+                ?>
+                </div> 
+                <div class="d-flex justify-content-between">
+                    <span class="padraoRotulo" style="padding-right:5px; padding-left:5px; color:#a1a1a1; font-size:14px; display:<?=(($c->padrao == '1')?'block':'none')?>">Padrão</span>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input padrao" type="radio" name="padrao" role="switch" value="<?=$c->codigo?>" <?=(($c->padrao == '1')?'checked':false)?> id="flexSwitchCheckDefault<?=$c->codigo?>">
+                    </div>
+                </div>
+            <?php
+
+                        }
+                        echo " Total: ". $total."<br>";
+                    }
+            ?>
+        </div>
+  </div>
+    </div>
+</div>
+
+
+<script>
+    $(function(){
+
+
+    })
+</script>
