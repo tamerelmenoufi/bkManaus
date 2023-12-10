eabea0cfa3a0c4b76855bb9a687c74bd0b7989a4
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-10 14:41:23 +0200

    atualizações

--- app/home/banner.php
+++ app/home/banner.php
@@ -40,11 +40,11 @@
         <div style="position:relative; background:orange;">
             <img src="img/transparente.png" style="width:100%; position:relative;" />
             <div style="position:absolute; left:0, right:0; bottom:30px; top:70px; z-index:10">
-                <div class="row">
-                    <div class="col-12"><center><img src="img/banner.png?<?=$md5?>" style="width:70%;" /></center></div>
-                    <div class="col-12 text-center" style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold;"><?=$d->produto?></div>
-                    <div class="col-6" style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold; border:solid black 1px">R$</div>
-                    <div class="col-6" style="font-size:70px; color:#fff; font-family:FlameBold; border:solid black 1px; padding:0;">49<span style="font-size:25px; color:#fff; font-family:FlameBold;">99</span></div>
+                <div class="d-flex justify-content-center"><img src="img/banner.png?<?=$md5?>" style="width:70%;" /></div>
+                <div class="d-flex justify-content-center" style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold;"><?=$d->produto?></div>
+                <div class="d-flex justify-content-center">
+                    <span style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold; border:solid black 1px">R$</span>
+                    <span style="font-size:70px; color:#fff; font-family:FlameBold; border:solid black 1px; padding:0;">49<span style="font-size:25px; color:#fff; font-family:FlameBold;">99</span></span>
                 </div>
             </div>
         </div>
