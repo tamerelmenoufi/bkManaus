097563b3208a3e46a1bcb1f26b1ad7dcc49a16ac
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-10 14:42:34 +0200

    atualizações

--- app/home/banner.php
+++ app/home/banner.php
@@ -42,7 +42,7 @@
             <div style="position:absolute; left:0, right:0; bottom:30px; top:70px; z-index:10">
                 <div class="d-flex justify-content-center"><img src="img/banner.png?<?=$md5?>" style="width:70%;" /></div>
                 <div class="d-flex justify-content-center" style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold;"><?=$d->produto?></div>
-                <div class="d-flex justify-content-center">
+                <div class="d-flex justify-content-center align-items-start">
                     <span style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold; border:solid black 1px">R$</span>
                     <span style="font-size:70px; color:#fff; font-family:FlameBold; border:solid black 1px; padding:0;">49<span style="font-size:25px; color:#fff; font-family:FlameBold;">99</span></span>
                 </div>
