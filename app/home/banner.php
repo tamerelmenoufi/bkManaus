3bad87b376694f5d17c1db235cc4f5632b899f53
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-10 14:00:47 +0200

    atualizações

--- app/home/banner.php
+++ app/home/banner.php
@@ -37,8 +37,8 @@
             <div style="position:absolute; left:0, right:0; bottom:30px; top:30px; z-index:10">
                 <div class="row">
                     <div class="col-12 text-center"><img src="img/banner.png?<?=$md5?>" style="width:90%;" /></div>
-                    <div class="col-6" style="color:#fff; font-size:20px; text-align:right; font-family:FlameBold;">Combo<p>R$</p></div>
-                    <div class="col-6" style="font-size:70px; color:#fff; font-family:FlameBold;">49<span style="font-size:20px; color:#fff; font-family:FlameBold;">99</span></div>
+                    <div class="col-6" style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold;">Combo<p>R$</p></div>
+                    <div class="col-6" style="font-size:70px; color:#fff; font-family:FlameBold;">49<span style="font-size:25px; color:#fff; font-family:FlameBold;">99</span></div>
                 </div>
             </div>
         </div>
