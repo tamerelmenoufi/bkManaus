8143f1c7b0905318cbb5824f39a4c4ab70bbf791
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-10 17:26:56 +0200

    atualizações

---
+++ app/pedido/pagamento.php
@@ -0,0 +1,71 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+    if($_POST['idUnico']){
+        $_SESSION['idUnico'] = $_POST['idUnico'];
+    }
+
+    if($_POST['codUsr']){
+        $_SESSION['codUsr'] = $_POST['codUsr'];
+        $where = " where codigo = '{$_SESSION['codUsr']}'";
+    }
+
+    $query = "select * from clientes where codigo = '{$_POST['codUsr']}'";
+
+    $result = mysqli_query($con, $query);
+
+    $d = mysqli_fetch_object($result);
+
+?>
+
+<style>
+    .enderecoLabel{
+        white-space: nowrap;
+        overflow: hidden; /* "overflow" value must be different from "visible" */
+        text-overflow: ellipsis;
+        color:#333;
+        font-size:14px;
+        cursor:pointer;
+    }
+</style>
+
+<div class="row g-0 p-2">
+    <div class="card p-2">
+        <h4 class="w-100 text-center">Pagamento</h4>
+
+            <div class="d-flex justify-content-between">    
+                <div class="enderecoLabel w-100" >
+                    <i class="fa-solid fa-location-dot"></i>
+                    Total da compra
+                </div>
+                <span total></span> 
+            </div>  
+            <div class="d-flex justify-content-between">    
+                <div class="enderecoLabel w-100" >
+                    <i class="fa-solid fa-location-dot"></i>
+                    Taxa de Entrega
+                </div>
+                <span taxa_entraga>R$ 12,00</span> 
+            </div>
+            <div class="d-flex justify-content-between">    
+                <div class="enderecoLabel w-100" >
+                    <i class="fa-solid fa-location-dot"></i>
+                    Total a Pagar
+                </div>
+                <span pagar>R$ 69,90,00</span> 
+            </div>
+            
+
+        
+    </div>
+</div>
+
+
+<script>
+    $(function(){
+
+        $("span").html($("div[total]").attr("total"))
+
+
+    })
+</script>
