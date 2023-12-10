ae6371e4a0fc92ed4b1c209cbce3f6bf384d7dcb
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-10 16:49:40 +0200

    atualizações

--- app/home/banner.php
+++ app/home/banner.php
@@ -51,7 +51,7 @@
             <img src="<?=$capa?>" style="width:100%; position:relative;" />
             <div style="position:absolute; left:0, right:0; bottom:30px; top:50px; z-index:10; width:100%; border:1px solid green;">
                 <div class="d-flex justify-content-center" style=" border:1px solid green;"><img src="<?=$icon?>" style="width:70%;" /></div>
-                <div class="d-flex justify-content-center" style="color:#fff; font-size:23px; text-align:right; font-family:FlameBold; margin-top:-20px;"><?=$d->produto?></div>
+                <div class="d-flex justify-content-center" style="color:#fff; font-size:23px; text-align:right; font-family:FlameBold; margin-top:-10px;"><?=$d->produto?></div>
                 <div class="d-flex justify-content-center align-items-end">
                     <div style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold; padding:20px;">R$</div>
                     <div class="d-flex justify-content-center align-items-start" style="font-size:70px; color:#fff; font-family:FlameBold; margin-top:-25px;">49<p style="font-size:25px; color:#fff; font-family:FlameBold; padding-top:20px;">99</p></div>
