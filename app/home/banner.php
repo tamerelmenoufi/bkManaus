615d425e891033178a21ea9f358d05ee6d0c118a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-10 14:06:24 +0200

    atualizações

--- app/home/banner.php
+++ app/home/banner.php
@@ -42,6 +42,26 @@
                 </div>
             </div>
         </div>
+        <div style="position:relative; background:red;">
+            <img src="img/transparente.png" style="width:100%; position:relative;" />
+            <div style="position:absolute; left:0, right:0; bottom:30px; top:70px; z-index:10">
+                <div class="row">
+                    <div class="col-12"><center><img src="img/banner.png?<?=$md5?>" style="width:70%;" /></center></div>
+                    <div class="col-6" style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold;">Combo<p>R$</p></div>
+                    <div class="col-6" style="font-size:70px; color:#fff; font-family:FlameBold;">49<span style="font-size:25px; color:#fff; font-family:FlameBold;">99</span></div>
+                </div>
+            </div>
+        </div>
+        <div style="position:relative; background:black;">
+            <img src="img/transparente.png" style="width:100%; position:relative;" />
+            <div style="position:absolute; left:0, right:0; bottom:30px; top:70px; z-index:10">
+                <div class="row">
+                    <div class="col-12"><center><img src="img/banner.png?<?=$md5?>" style="width:70%;" /></center></div>
+                    <div class="col-6" style="color:#fff; font-size:25px; text-align:right; font-family:FlameBold;">Combo<p>R$</p></div>
+                    <div class="col-6" style="font-size:70px; color:#fff; font-family:FlameBold;">49<span style="font-size:25px; color:#fff; font-family:FlameBold;">99</span></div>
+                </div>
+            </div>
+        </div>
 
     </div>
     <div class="d-flex justify-content-center barra_banner">
