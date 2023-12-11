081e66590cfa3e9efc879bba4fb3eeee2db4d050
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-11 18:40:17 +0200

    atualizações

--- lib/includes.php
+++ lib/includes.php
@@ -3,6 +3,7 @@
     include("/appinc/connect.php");
     include("fn.php");
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/vendor/mottu/classes.php");
+    include("AppWapp.php");
     $con = AppConnect('bk_manaus');
     $conApi = AppConnect('information_schema');
     $md5 = md5(date("YmdHis"));
