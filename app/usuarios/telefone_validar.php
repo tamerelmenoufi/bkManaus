836d39b0caedacddad6d633d31cbe0c03f94afe2
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-11 18:50:47 +0200

    atualizações

--- app/usuarios/telefone_validar.php
+++ app/usuarios/telefone_validar.php
@@ -1,1 +1,13 @@
-{"status":"success", "codigo":"1122"}
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+    $d1 = rand(1,9);
+    $d2 = rand(0,9);
+    $d3 = rand(0,9);
+    $d4 = rand(0,9);
+
+    $cod = $d1.$d2.$d3.$d4;
+
+    $result = EnviarWapp($num,"BK Manaus informe: Seu código de acesso é *{$cod}*");
+
+echo "{\"status\":\"success\", \"codigo\":\"{$cod}\"}";
