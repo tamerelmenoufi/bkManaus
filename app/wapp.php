b02ac3d22ee72cc4e860410e4b00cecea1bb0035
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-13 01:37:38 +0200

    atualizações

--- app/wapp.php
+++ app/wapp.php
@@ -5,6 +5,7 @@
 
 // curl_setopt($ch, CURLOPT_URL, 'https://apinew.socialhub.pro/api/sendGroupMessage');
 // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Skip SSL Verification
 // curl_setopt($ch, CURLOPT_POST, 1);
 // $post = array(
 //     'api_token' => '\"SEU API_TOKEN AQUI\"',
@@ -27,6 +28,7 @@ $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, 'https://apinew.socialhub.pro/api/sendMessage');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Skip SSL Verification
 curl_setopt($ch, CURLOPT_POST, 1);
 $post = array(
     'api_token' => '\"cehsUsTLkMgHyOg079WCrSmLiuopNbjx\"',
