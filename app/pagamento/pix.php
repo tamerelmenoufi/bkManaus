51363ea5f237f313c6198e3677ef76818e1e96f3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-12 21:05:47 +0200

    atualizações

--- app/pagamento/pix.php
+++ app/pagamento/pix.php
@@ -76,163 +76,159 @@
  
 </style>
 
-<div class="col">
-    <div class="row">
-            <div class="col-12">
-                <div class="card mb-3" style="background-color:#fafcff; padding:20px;">
-                    <p style="text-align:center">
-                        <?php
 
-                            $pedido = str_pad($v->codigo, 6, "0", STR_PAD_LEFT);
+<div class="card mb-3" style="background-color:#fafcff; padding:20px;">
+        <p style="text-align:center">
+            <?php
+
+                $pedido = str_pad($v->codigo, 6, "0", STR_PAD_LEFT);
 
 
-                            $PIX = new MercadoPago;
-                            $retorno = $PIX->ObterPagamento($v->operadora_id); //////////////
-                            $operadora_retorno = $retorno;
-                            $dados = json_decode($retorno);
+                $PIX = new MercadoPago;
+                $retorno = $PIX->ObterPagamento($v->operadora_id); //////////////
+                $operadora_retorno = $retorno;
+                $dados = json_decode($retorno);
 
-                            if( $v->operadora_id and
-                                $d->pagamento == 'pix' and
-                                $v->valor_total == $dados->transaction_amount
-                                ){
+                if( $v->operadora_id and
+                    $d->pagamento == 'pix' and
+                    $v->valor_total == $dados->transaction_amount
+                    ){
 
-                                // echo "<pre>";
-                                // print_r($dados);
-                                // echo "</pre>";
-                                $operadora_id = $dados->id;
-
-
-                                $forma_pagamento = $dados->payment_method_id;
-                                $operadora_situacao = $dados->status;
-                                $qrcode = $dados->point_of_interaction->transaction_data->qr_code;
-                                $qrcode_img = $dados->point_of_interaction->transaction_data->qr_code_base64;
-
-                            }else{
-
-                                //AQUI É A GERAÇÃO DA COBRANÇA PIX
-
-                                $PIX = new MercadoPago;
-                                // "transaction_amount": '.$d->total.',
-                                // "transaction_amount": 2.11,
+                    // echo "<pre>";
+                    // print_r($dados);
+                    // echo "</pre>";
+                    $operadora_id = $dados->id;
 
 
-                                $json = '{
-                                    "transaction_amount": '.$v->valor_total.',
-                                    "description": "Pedido '.$pedido.' - Venda BKManaus (Delivery)",
-                                    "payment_method_id": "pix",
-                                    "payer": {
-                                    "email": "'.$d->Cemail.'",
-                                    "first_name": "'.substr($d->Cnome, 0, ($pos-1)).'",
-                                    "last_name": "'.substr($d->Cnome, $pos, strlen($d->Cnome)).'",
-                                    "identification": {
-                                        "type": "CPF",
-                                        "number": "'.str_replace(array('.','-'),false,$d->Ccpf).'"
-                                    },
-                                    "address": {
-                                        "zip_code": "'.str_replace(array('.','-'),false,$d->Ccep).'",
-                                        "street_name": "'.$d->Clogradouro.'",
-                                        "street_number": "'.$d->Cnumero.'",
-                                        "neighborhood": "'.$d->Cbairro.'",
-                                        "city": "Manaus",
-                                        "federal_unit": "AM"
-                                    }
-                                    }
-                                }';
-                                $retorno = $PIX->Transacao($json);
-                                // echo "<hr>";
-                                $operadora_retorno = $retorno;
+                    $forma_pagamento = $dados->payment_method_id;
+                    $operadora_situacao = $dados->status;
+                    $qrcode = $dados->point_of_interaction->transaction_data->qr_code;
+                    $qrcode_img = $dados->point_of_interaction->transaction_data->qr_code_base64;
 
-                                $dados = json_decode($retorno);
+                }else{
+
+                    //AQUI É A GERAÇÃO DA COBRANÇA PIX
+
+                    $PIX = new MercadoPago;
+                    // "transaction_amount": '.$d->total.',
+                    // "transaction_amount": 2.11,
 
 
-                                // print_r($dados);
-                                // exit();
-                                $operadora_id = $dados->id;
-                                $forma_pagamento = $dados->payment_method_id;
-                                $operadora_situacao = $dados->status;
-                                $qrcode = $dados->point_of_interaction->transaction_data->qr_code;
-                                $qrcode_img = $dados->point_of_interaction->transaction_data->qr_code_base64;
-                                $api_delivery = false;
+                    $json = '{
+                        "transaction_amount": '.$v->valor_total.',
+                        "description": "Pedido '.$pedido.' - Venda BKManaus (Delivery)",
+                        "payment_method_id": "pix",
+                        "payer": {
+                        "email": "'.$d->Cemail.'",
+                        "first_name": "'.substr($d->Cnome, 0, ($pos-1)).'",
+                        "last_name": "'.substr($d->Cnome, $pos, strlen($d->Cnome)).'",
+                        "identification": {
+                            "type": "CPF",
+                            "number": "'.str_replace(array('.','-'),false,$d->Ccpf).'"
+                        },
+                        "address": {
+                            "zip_code": "'.str_replace(array('.','-'),false,$d->Ccep).'",
+                            "street_name": "'.$d->Clogradouro.'",
+                            "street_number": "'.$d->Cnumero.'",
+                            "neighborhood": "'.$d->Cbairro.'",
+                            "city": "Manaus",
+                            "federal_unit": "AM"
+                        }
+                        }
+                    }';
+                    $retorno = $PIX->Transacao($json);
+                    // echo "<hr>";
+                    $operadora_retorno = $retorno;
 
-                                if($operadora_id){
-
-                                    // //////////////////////API DELIVERY////////////////////////////
-                                    // if($dados->status == 'approved' and $d->retirada_local != '1'){
-                                    //     $json = '{
-                                    //         "code": "'.$v->codigo.'",
-                                    //         "fullCode": "bk-'.$v->codigo.'",
-                                    //         "preparationTime": 0,
-                                    //         "previewDeliveryTime": false,
-                                    //         "sortByBestRoute": false,
-                                    //         "deliveries": [
-                                    //           {
-                                    //             "code": "'.$v->codigo.'",
-                                    //             "confirmation": {
-                                    //               "mottu": true
-                                    //             },
-                                    //             "name": "'.$d->Cnome.'",
-                                    //             "phone": "'.trim(str_replace(array(' ','-','(',')'), false, $d->Ctelefone)).'",
-                                    //             "observation": "'.$d->Ccomplemento.'",
-                                    //             "address": {
-                                    //               "street": "'.$d->Clogradouro.'",
-                                    //               "number": "'.$d->Cnumero.'",
-                                    //               "complement": "'.$d->Cponto_referencia.'",
-                                    //               "neighborhood": "'.$d->Cbairro.'",
-                                    //               "city": "Manaus",
-                                    //               "state": "AM",
-                                    //               "zipCode": "'.trim(str_replace(array(' ','-'), false, $d->Ccep)).'"
-                                    //             },
-                                    //             "onlinePayment": true,
-                                    //             "productValue": '.$v->valor_total.'
-                                    //           }
-                                    //         ]
-                                    //       }';
-
-                                    //     $mottu = new mottu;
-
-                                    //     $retorno1 = $mottu->NovoPedido($json, $d->id_mottu);
-                                    //     $retorno = json_decode($retorno1);
-
-                                    //     $api_delivery = $retorno->id;
-                                    // }
-                                    // //////////////////////API DELIVERY////////////////////////////
+                    $dados = json_decode($retorno);
 
 
-                                    $q = "update vendas set
-                                                                pagamento = 'pix',
-                                                                pix_detalhes = '".(($retorno)?:'{}')."',
-                                                                delivery = 'mottu',
-                                                                delivery_detalhes = '".(($retorno1)?:'{}')."'
-                                                                ".(($api_delivery or $d->retirada_local == '1')?", situacao = 'pago'":false)."
-                                                        where codigo = '{$v->codigo}'
-                                                ";
-                                    
-                                    mysqli_query($con, $q);
+                    // print_r($dados);
+                    // exit();
+                    $operadora_id = $dados->id;
+                    $forma_pagamento = $dados->payment_method_id;
+                    $operadora_situacao = $dados->status;
+                    $qrcode = $dados->point_of_interaction->transaction_data->qr_code;
+                    $qrcode_img = $dados->point_of_interaction->transaction_data->qr_code_base64;
+                    $api_delivery = false;
 
-                                }
-                            }
+                    if($operadora_id){
 
-                            // $qrcode = '12e44a26-e3b4-445f-a799-1199df32fa1e';
-                            // $operadora_id = 23997683882;
+                        // //////////////////////API DELIVERY////////////////////////////
+                        // if($dados->status == 'approved' and $d->retirada_local != '1'){
+                        //     $json = '{
+                        //         "code": "'.$v->codigo.'",
+                        //         "fullCode": "bk-'.$v->codigo.'",
+                        //         "preparationTime": 0,
+                        //         "previewDeliveryTime": false,
+                        //         "sortByBestRoute": false,
+                        //         "deliveries": [
+                        //           {
+                        //             "code": "'.$v->codigo.'",
+                        //             "confirmation": {
+                        //               "mottu": true
+                        //             },
+                        //             "name": "'.$d->Cnome.'",
+                        //             "phone": "'.trim(str_replace(array(' ','-','(',')'), false, $d->Ctelefone)).'",
+                        //             "observation": "'.$d->Ccomplemento.'",
+                        //             "address": {
+                        //               "street": "'.$d->Clogradouro.'",
+                        //               "number": "'.$d->Cnumero.'",
+                        //               "complement": "'.$d->Cponto_referencia.'",
+                        //               "neighborhood": "'.$d->Cbairro.'",
+                        //               "city": "Manaus",
+                        //               "state": "AM",
+                        //               "zipCode": "'.trim(str_replace(array(' ','-'), false, $d->Ccep)).'"
+                        //             },
+                        //             "onlinePayment": true,
+                        //             "productValue": '.$v->valor_total.'
+                        //           }
+                        //         ]
+                        //       }';
 
-                        ?>
-                        Utilize o QrCode para pagar a sua conta ou copie o códio PIX abaixo.
-                    </p>
-                    <div>
-                        <img src="data:image/png;base64,<?=$qrcode_img?>" style="width:100%">
-                        <!-- <img src="img/qrteste.png" style="width:100%"> -->
-                        <div class="status_pagamento"></div>
-                    </div>
-                    Total a Pagar:
-                    <h1>R$ <?=number_format($v->valor_total,2,'.',false)?></h1>
-                    <p style="text-align:center; font-size:12px;">Clique no botão abaixo para copiar o Código PIX de sua compra.</p>
-                    <!-- <p style="text-align:center; font-size:16px;"><?=$qrcode?></p> -->
-                    <button copiar="<?=$qrcode?>" class="btn btn-secondary btn-lg btn-block"><i class="fa-solid fa-copy"></i> <span>Copiar Código PIX</span></button>
-                </div>
-            </div>
+                        //     $mottu = new mottu;
+
+                        //     $retorno1 = $mottu->NovoPedido($json, $d->id_mottu);
+                        //     $retorno = json_decode($retorno1);
+
+                        //     $api_delivery = $retorno->id;
+                        // }
+                        // //////////////////////API DELIVERY////////////////////////////
+
+
+                        $q = "update vendas set
+                                                    pagamento = 'pix',
+                                                    pix_detalhes = '".(($retorno)?:'{}')."',
+                                                    delivery = 'mottu',
+                                                    delivery_detalhes = '".(($retorno1)?:'{}')."'
+                                                    ".(($api_delivery or $d->retirada_local == '1')?", situacao = 'pago'":false)."
+                                            where codigo = '{$v->codigo}'
+                                    ";
+                        
+                        mysqli_query($con, $q);
+
+                    }
+                }
+
+                // $qrcode = '12e44a26-e3b4-445f-a799-1199df32fa1e';
+                // $operadora_id = 23997683882;
+
+            ?>
+            Utilize o QrCode para pagar a sua conta ou copie o códio PIX abaixo.
+        </p>
+        <div>
+            <img src="data:image/png;base64,<?=$qrcode_img?>" style="width:100%">
+            <!-- <img src="img/qrteste.png" style="width:100%"> -->
+            <div class="status_pagamento"></div>
         </div>
+        Total a Pagar:
+        <h1>R$ <?=number_format($v->valor_total,2,'.',false)?></h1>
+        <p style="text-align:center; font-size:12px;">Clique no botão abaixo para copiar o Código PIX de sua compra.</p>
+        <!-- <p style="text-align:center; font-size:16px;"><?=$qrcode?></p> -->
+        <button copiar="<?=$qrcode?>" class="btn btn-secondary btn-lg btn-block"><i class="fa-solid fa-copy"></i> <span>Copiar Código PIX</span></button>
     </div>
 </div>
+
 <script>
     $(function(){
 
