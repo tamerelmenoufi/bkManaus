fb16cb6a477adbf82e2d102fd263f8cf2214f55d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-13 09:35:12 +0200

    atualizações

--- app/wapp.php
+++ app/wapp.php
@@ -31,10 +31,10 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Skip SSL Verification
 curl_setopt($ch, CURLOPT_POST, 1);
 $post = array(
-    'api_token' => "",
+    'api_token' => "cehsUsTLkMgHyOg079WCrSmLiuopNbjx",
     'phone' => "+201116680996",
     // 'file' => '@' .realpath('\"DIRETÓRIO DO ARQUIVO\"'),
-    'message' => "Enviando a mensagem para validação da API de teste conforme as especificações - fase 2."
+    'message' => "Enviando a mensagem para validação da API de teste conforme as especificações - fase 3."
 );
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 
