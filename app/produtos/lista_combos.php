58c6d2b3e3636765c9a13a430c49908fe4b9f7db
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-14 16:16:15 +0200

    atualizações

--- app/produtos/lista_combos.php
+++ app/produtos/lista_combos.php
@@ -118,7 +118,7 @@ while($d = mysqli_fetch_object($result)){
             $prd[] = $d1->produto;
         }
 
-        $prd = implode("</div><div>- ", $prd);
+        $prd = implode("</div><div style='color:".(($d->promocao == '1')?'#ffffff':'#000000')."';>- ", $prd);
     }
 ?>
     <div class="produto_painel" codigo = "<?=$d->codigo?>" style="background-color:<?=(($d->promocao == '1')?'#bd0100':'trasparent')?>">
@@ -137,7 +137,7 @@ while($d = mysqli_fetch_object($result)){
                 </h2>
             </div>     -->
             <div class="produto_dados">
-                <div class="promocao">DE R$ <?=number_format($d->valor,2,",",false)?></div>
+                <div class="promocao">DE R$ <?=number_format(CalculaValorCombo($d->codigo),2,",",false)?></div>
                 <div class="d-flex justify-content-between w-100" style="color:<?=(($d->promocao == '1')?'#fbdb00':'#f4352b')?>;">
                     <div><h2 class="produto_dados" style="margin-top:2px;"><span style="color:#fbdb00; font-size:12px;"><?=(($d->promocao == '1')?'POR ':false)?></span>R$ <?=number_format((($d->promocao == '1')?$d->valor_promocao:CalculaValorCombo($d->codigo)),2,",",false)?></h2></div>
                     <i class="fa-solid fa-circle-play me-3" style="font-size:25px;"></i>
@@ -145,6 +145,27 @@ while($d = mysqli_fetch_object($result)){
             </div>         
         </div>
     </div>
+
+
+
+    <!-- <div class="produto_painel" codigo="<?=$d->codigo?>" style="background-color:<?=(($d->promocao == '1')?'#bd0100':'trasparent')?>">
+        <img src="<?=$icon?>" />
+        <div class="w-100">
+            <div class="produto_dados">
+                <h4 style="color:<?=(($d->promocao == '1')?'#fbdb00':'#600f0b')?>"><?=$d->produto?></h4>
+            </div>
+            <div class="produto_dados" style="height:60px;">
+                <div style="color:<?=(($d->promocao == '1')?'#ffffff':'#000000')?>"><?=$d->descricao?></div>
+            </div>
+            <div class="produto_dados">
+                <div class="promocao">DE R$ <?=number_format($d->valor,2,",",false)?></div>
+                <div class="d-flex justify-content-between w-100" style="color:<?=(($d->promocao == '1')?'#fbdb00':'#f4352b')?>;">
+                    <div><h2 class="produto_dados" style="margin-top:2px;"><span style="color:#fbdb00; font-size:12px;"><?=(($d->promocao == '1')?'POR ':false)?></span>R$ <?=number_format((($d->promocao == '1')?$d->valor_promocao:$d->valor),2,",",false)?></h2></div>
+                    <i class="fa-solid fa-circle-play me-3" style="font-size:25px;"></i>
+                </div>
+            </div>            
+        </div>
+    </div> -->
 
 <?php
 }
