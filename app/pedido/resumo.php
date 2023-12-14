a9cabe58c3599de8ca302267740254edb29e3cd3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-14 16:33:03 +0200

    atualizações

--- app/pedido/resumo.php
+++ app/pedido/resumo.php
@@ -42,14 +42,14 @@
         top:0;
         width:100%;
         height:100px;
-        background-color:#ffc63a;
+        background-color:#f4000a;
         color:#670600;
         border-bottom-right-radius:40px;
         border-bottom-left-radius:40px;
         font-family:FlameBold;
     }
     .barra_topo h2{
-        
+        color:#f6e13a;
     }
 
     .home_corpo{
