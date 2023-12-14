f51b0850e2e039814c026c60996b3d5da0c71007
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-14 15:36:21 +0200

    atualizações

--- app/produtos/lista_produtos.php
+++ app/produtos/lista_produtos.php
@@ -121,7 +121,7 @@ while($d = mysqli_fetch_object($result)){
             <div class="produto_dados">
                 <div class="promocao">DE R$ <?=number_format($d->valor,2,",",false)?></div>
                 <div class="d-flex justify-content-between w-100" style="color:<?=(($d->promocao == '1')?'#fbdb00':'#f4352b')?>;">
-                    <div><h2 class="produto_dados" style="margin-top:5px;"><span style="color:#fbdb00; font-size:12px;"><?=(($d->promocao == '1')?'POR ':false)?></span>R$ <?=number_format((($d->promocao == '1')?$d->valor_promocao:$d->valor),2,",",false)?></h2></div>
+                    <div><h2 class="produto_dados" style="margin-top:0px;"><span style="color:#fbdb00; font-size:12px;"><?=(($d->promocao == '1')?'POR ':false)?></span>R$ <?=number_format((($d->promocao == '1')?$d->valor_promocao:$d->valor),2,",",false)?></h2></div>
                     <i class="fa-solid fa-circle-play me-3" style="font-size:25px;"></i>
                 </div>
             </div>            
