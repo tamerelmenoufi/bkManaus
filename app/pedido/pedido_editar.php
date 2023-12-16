7e4e5d088a70a29ab849a1c656a745ac65230e1d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-16 21:06:55 +0200

    atualizações

--- app/pedido/pedido_editar.php
+++ app/pedido/pedido_editar.php
@@ -66,6 +66,38 @@
 </style>
 
 
+<div class="row g-0 p-2 mt-3">
+    <div class="card p-2">
+        <h4 class="w-100 text-center">RESUMO DO PEDIDO</h4>
+                <?php
+
+                    foreach(json_decode($d->detalhes) as $i => $dados){
+
+                        $pd = mysqli_fetch_object(mysqli_query($con, "select * from produtos where codigo = '{$dados->codigo}'"));
+                        if($dados->status){
+                ?>
+            <div class="d-flex justify-content-between">    
+                <div class="enderecoLabel w-70" codigo="<?=$c->codigo?>">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <?=$dados->quantidade?> x <?=$pd->produto?>
+                </div> 
+                <div class="d-flex justify-content-between">
+                    <!-- <div>R$ <?=number_format($dados->total,2,',',false)?></div> -->
+                    <div class="w-100 text-end">R$ <?=number_format($dados->total*$dados->quantidade,2,',',false)?></div>
+                </div>
+            </div>    
+            <?php
+                $total = ($total + ($dados->total*$dados->quantidade));
+
+                        }
+                    }
+            ?>
+            <div class="w-100 text-end" total="<?=$total?>"><b>TOTAL R$ <?=number_format($total,2,',',false)?></b></div>
+        
+    </div>
+</div>
+
+
 <?php
     foreach(json_decode($d->detalhes) as $i => $dados){
         $pd = mysqli_fetch_object(mysqli_query($con, "select * from produtos where codigo = '{$dados->codigo}'"));
