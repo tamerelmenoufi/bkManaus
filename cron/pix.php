d461f1f2f74ca8baa44885d3116610491ec50896
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-16 16:49:28 +0200

    atualizações

--- cron/pix.php
+++ cron/pix.php
@@ -7,27 +7,24 @@
             'pending' => 'pendente',
             'cancelled' => 'cancelado'
         ];
-        return $opc[$e];
+        return (($opc[$e])?:$e);
     }
 
     $query = "select *, pix_detalhes->>'$.id' as operadora_id from vendas where situacao = 'pendente'";
     $result = mysqli_query($con, $query);
     while($v = mysqli_fetch_object($result)){
 
-
-        $pedido = str_pad($v->codigo, 6, "0", STR_PAD_LEFT);
         $PIX = new MercadoPago;
         $retorno = $PIX->ObterPagamento($v->operadora_id);
         $operadora_retorno = $retorno;
         $dados = json_decode($retorno);
 
-        echo $q = "update vendas set
+        $q = "update vendas set
             pagamento = 'pix',
             pix_detalhes = '".(($retorno)?:'{}')."',
             situacao = '".SituacaoPIX($dados->status)."'
-            where codigo = '{$v->codigo}'
-        <br>";
+            where codigo = '{$v->codigo}'";
 
-        //mysqli_query($con, $q);
+        mysqli_query($con, $q);
 
     }
