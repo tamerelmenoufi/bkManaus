c520a82805dafcc1c536ef2675afed9a739378d0
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-16 16:36:34 +0200

    atualizações

--- cron/pix.php
+++ cron/pix.php
@@ -10,7 +10,7 @@
         return $opc[$e];
     }
 
-    $query = "select * from vendas where situacao = 'pendente'";
+    $query = "select *, pix_detalhes->>'$.id' as operadora_id from vendas where situacao = 'pendente'";
     $result = mysqli_query($con, $query);
     while($v = mysqli_fetch_object($result)){
 
