a831e07c4e391ccec151a988ad3c58fbf6d5452b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-16 16:32:00 +0200

    atualizações

--- cron/pix.php
+++ cron/pix.php
@@ -10,24 +10,26 @@
         return $opc[$e];
     }
 
-
-    $v = mysqli_fetch_object(mysqli_query($con, "select *, pix_detalhes->>'$.id' as operadora_id from vendas where codigo = '{$_SESSION['codVenda']}'"));
-
-
-
-    $pedido = str_pad($v->codigo, 6, "0", STR_PAD_LEFT);
-    $PIX = new MercadoPago;
-    $retorno = $PIX->ObterPagamento($v->operadora_id);
-    $operadora_retorno = $retorno;
-    $dados = json_decode($retorno);
+    $query = "select * from vendas situacao = 'pendente'";
+    $result = mysqli_query($con, $query);
+    while($v = mysqli_fetch_object($result)){
 
 
+        $pedido = str_pad($v->codigo, 6, "0", STR_PAD_LEFT);
+        $PIX = new MercadoPago;
+        $retorno = $PIX->ObterPagamento($v->operadora_id);
+        $operadora_retorno = $retorno;
+        $dados = json_decode($retorno);
 
-    echo $q = "update vendas set
-        pagamento = 'pix',
-        pix_detalhes = '".(($retorno)?:'{}')."',
-        situacao = '".SituacaoPIX($dados->ststus)."'
-        where codigo = '{$v->codigo}'
-    ";
 
-    //mysqli_query($con, $q);
+
+        echo $q = "update vendas set
+            pagamento = 'pix',
+            pix_detalhes = '".(($retorno)?:'{}')."',
+            situacao = '".SituacaoPIX($dados->ststus)."'
+            where codigo = '{$v->codigo}'
+        ";
+
+        //mysqli_query($con, $q);
+
+    }
