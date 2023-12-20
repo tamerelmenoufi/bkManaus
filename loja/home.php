f323de65c13cd140545ba779c7ccfceff2f8a687
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-12-20 09:18:42 +0200

    atualizações

--- loja/home.php
+++ loja/home.php
@@ -49,7 +49,7 @@
 
             <ul class="list-group m-3">
                 <?php
-                $query = "select * from vendas where mottu = '{$d->delivery_id}' and situacao = 'pago' order by data desc";
+                echo $query = "select * from vendas where mottu = '{$d->delivery_id}' and situacao = 'pago' order by data desc";
                 $result = mysqli_query($con, $query);
                 while($d = mysqli_fetch_object($result)){
                 ?>
