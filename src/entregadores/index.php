a2be00acbeb4b9a8f7ee2e6ad6a118d3a38708bb
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-05 14:11:48 +0200

    atualizações

--- src/entregadores/index.php
+++ src/entregadores/index.php
@@ -129,7 +129,7 @@
                     <td>
 
                     <div class="form-check form-switch">
-                      <input class="form-check-input situacao" type="checkbox" <?=(($d->codigo == 1)?'disabled':false)?> <?=(($d->situacao)?'checked':false)?> usuario="<?=$d->codigo?>">
+                      <input class="form-check-input situacao" type="checkbox" <?=(($d->situacao)?'checked':false)?> usuario="<?=$d->codigo?>">
                     </div>
 
                     </td>
@@ -144,15 +144,11 @@
                       >
                         Editar
                       </button>
-                      <?php
-                      if($d->codigo != 1){
-                      ?>
+
                       <button class="btn btn-danger" delete="<?=$d->codigo?>">
                         Excluir
                       </button>
-                      <?php
-                      }
-                      ?>
+
                     </td>
                   </tr>
                   <?php
