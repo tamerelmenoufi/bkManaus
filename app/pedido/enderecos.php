f630bfb929b3bb2dc9b75fe09bb61d31baa5936f
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-11 23:37:27 +0200

    atualizações

--- app/pedido/enderecos.php
+++ app/pedido/enderecos.php
@@ -100,7 +100,7 @@
 
                 $local = json_decode($local);
 
-                echo $local->status."<br>";
+                echo $local->status."<br>".$local->routes[0]->legs[0]->distance->value."<br>";
 
                 if($local->status == 'OK'){
 
