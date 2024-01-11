6bb39e5ca7174f7d8a39aaa14a11a335c6d82b11
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-11 23:02:50 +0200

    atualizações

--- app/pedido/enderecos.php
+++ app/pedido/enderecos.php
@@ -39,7 +39,7 @@
                 $bairro = $c->bairro;
                 $localidade = $c->localidade;
                 $uf = $c->uf;
-                echo $coo = $c->coordenadas;
+                $coo = $c->coordenadas;
             }
         ?>
 
