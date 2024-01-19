8993553b535c6e8c645e10fb9d4affb26367d12a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-19 20:51:33 +0200

    atualizações

--- loja/entregadores/index.php
+++ loja/entregadores/index.php
@@ -51,116 +51,37 @@
         <div class="card">
           <h5 class="card-header">Lista de Entregadores</h5>
           <div class="card-body">
-            <div class="d-none d-md-block">
-              <div class="d-flex justify-content-between mb-3">
-                  <div class="input-group">
-                    <label class="input-group-text" for="inputGroupFile01">Buscar por </label>
-                    <input campoBusca type="text" class="form-control" value="<?=$_SESSION['usuarioBusca']?>" aria-label="Digite a informação para a busca">
-                    <button filtro="filtrar" class="btn btn-outline-secondary" type="button">Buscar</button>
-                    <button filtro="limpar" class="btn btn-outline-danger" type="button">limpar</button>
-                  </div>
+           
 
+            <div class="d-flex justify-content-between mb-3">
 
-                  <button
-                      novoCadastro
-                      class="btn btn-success btn-sm"
-                      data-bs-toggle="offcanvas"
-                      href="#offcanvasDireita"
-                      role="button"
-                      aria-controls="offcanvasDireita"
-                      style="margin-left:20px;"
-                  >Novo</button>
-              </div>
-            </div>
-
-            <div class="d-block d-md-none d-lg-none d-xl-none d-xxl-none">
-              <div class="d-flex justify-content-between mb-3">
-
-                  <div class="row">
-                    <div class="col-12 mb-2">
-                      <div class="input-group">
-                        <label class="input-group-text" for="inputGroupFile01">Buscar por </label>
-                        <input campoBusca type="text" class="form-control" value="<?=$_SESSION['usuarioBusca']?>" aria-label="Digite a informação para a busca">
-                      </div>
-                    </div>
-                    <div class="col-12 mb-2">
-                      <button filtro="filtrar" class="btn btn-outline-secondary w-100" type="button">Buscar</button>
-                    </div>
-                    <div class="col-12 mb-2">
-                      <button filtro="limpar" class="btn btn-outline-danger w-100" type="button">limpar</button>
-                    </div>
-                    <div class="col-12 mb-2">
-                      <button
-                          novoCadastro
-                          class="btn btn-success btn-sm w-100"
-                          data-bs-toggle="offcanvas"
-                          href="#offcanvasDireita"
-                          role="button"
-                          aria-controls="offcanvasDireita"
-                      >Novo</button>                      
+                <div class="row">
+                  <div class="col-12 mb-2">
+                    <div class="input-group">
+                      <label class="input-group-text" for="inputGroupFile01">Buscar por </label>
+                      <input campoBusca type="text" class="form-control" value="<?=$_SESSION['usuarioBusca']?>" aria-label="Digite a informação para a busca">
                     </div>
                   </div>
-              </div>
-            </div>
-
-            <div class="table-responsive d-none d-md-block">
-              <table class="table table-striped table-hover">
-                <thead>
-                  <tr>
-                    <th scope="col">Nome</th>
-                    <th scope="col">CPF</th>
-                    <th scope="col">Telefone</th>
-                    <th scope="col">Loja</th>
-                    <th scope="col">Situação</th>
-                    <th scope="col">Ações</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-                    $query = "select a.*, b.nome as loja_nome from entregadores a left join lojas b on a.loja = b.codigo where a.deletado != '1' {$where} order by a.nome asc";
-                    $result = sisLog($query);
-                    
-                    while($d = mysqli_fetch_object($result)){
-                  ?>
-                  <tr>
-                    <td class="w-100"><?=$d->nome?></td>
-                    <td><?=$d->cpf?></td>
-                    <td><?=$d->telefone?></td>
-                    <td><?=$d->loja_nome?></td>
-                    <td>
-
-                    <div class="form-check form-switch">
-                      <input class="form-check-input situacao" type="checkbox" <?=(($d->situacao)?'checked':false)?> usuario="<?=$d->codigo?>">
-                    </div>
-
-                    </td>
-                    <td>
-                      <button
-                        class="btn btn-primary"
-                        edit="<?=$d->codigo?>"
+                  <div class="col-12 mb-2">
+                    <button filtro="filtrar" class="btn btn-outline-secondary w-100" type="button">Buscar</button>
+                  </div>
+                  <div class="col-12 mb-2">
+                    <button filtro="limpar" class="btn btn-outline-danger w-100" type="button">limpar</button>
+                  </div>
+                  <div class="col-12 mb-2">
+                    <button
+                        novoCadastro
+                        class="btn btn-success btn-sm w-100"
                         data-bs-toggle="offcanvas"
                         href="#offcanvasDireita"
                         role="button"
                         aria-controls="offcanvasDireita"
-                      >
-                        Editar
-                      </button>
-
-                      <button class="btn btn-danger" delete="<?=$d->codigo?>">
-                        Excluir
-                      </button>
-
-                    </td>
-                  </tr>
-                  <?php
-                    }
-                  ?>
-                </tbody>
-              </table>
+                    >Novo</button>                      
+                  </div>
+                </div>
             </div>
 
 
-            <div class="d-block d-md-none d-lg-none d-xl-none d-xxl-none">
             <?php
                   $query = "select * from entregadores where deletado != '1' {$where} order by nome asc";
                   $result = sisLog($query);
@@ -221,7 +142,6 @@
                 <?php
                   }
                 ?>
-            </div>
 
 
           </div>
