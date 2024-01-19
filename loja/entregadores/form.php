d6e5a1cb51064e773353e8d416df73e33aac7753
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-19 21:01:58 +0200

    atualizações

--- loja/entregadores/form.php
+++ loja/entregadores/form.php
@@ -101,6 +101,7 @@
                             <div class="row">
                                 <div class="col">
                                     <div style="display:flex; justify-content:end">
+                                        <button type="button" class="btn btn-warning btn-ms me-3">Voltar</button>
                                         <button type="submit" class="btn btn-success btn-ms">Salvar</button>
                                         <input type="hidden" id="codigo" value="<?=$_POST['cod']?>" />
                                     </div>
