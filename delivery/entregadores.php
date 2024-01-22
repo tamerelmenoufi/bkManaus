0fb406d5b30d1a3210d0a96ad2bdf3b00ce5fbb4
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-22 14:25:16 +0200

    atualizações

--- delivery/entregadores.php
+++ delivery/entregadores.php
@@ -221,7 +221,7 @@
                         });                        
 
                     }else{
-                        $.alert("Ocorreu algum problema no seu acesso:<br><br>1. Dados não cadastrados no sistema<br>ou usuário logado em outro equipamento.<br>Favor entre em contato com a gerência de sua loja.");
+                        $.alert("Ocorreu algum problema no seu acesso:<br><br>1. Dados não cadastrados no sistema<br>2. Ou usuário logado em outro equipamento.<br><br>Favor entre em contato com a gerência de sua loja.");
                     }
 
                 }
