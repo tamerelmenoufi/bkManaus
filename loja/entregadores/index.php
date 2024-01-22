edac93d2881a5b610b3408fbed3f1ce2ef360d87
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-22 18:54:12 +0200

    atualizações

--- loja/entregadores/index.php
+++ loja/entregadores/index.php
@@ -53,7 +53,7 @@
             <h5 class="card-header">Lista de Entregadores</h5>
             <div class="card-body">
             
-            <div style="position:relative; width:100%; height:100%">
+            <div style="position:relative; width:100%; height:100%; border:solid 1px red">
               <div class="d-flex justify-content-between mb-3">
 
                   <div class="row">
