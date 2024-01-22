ef7454d7aeafc92b23d29450101b2239243fd623
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-22 19:27:27 +0200

    atualizações

--- loja/entregadores/index.php
+++ loja/entregadores/index.php
@@ -159,7 +159,7 @@
         Carregando('none');
 console.log($(".CorpoApp").height())
 
-        $(".card-body").css("height", ($(".CorpoApp").height() - 100))
+        $(".card-body").css("height", ($(".CorpoApp").height() - 150))
 
         $("button[novoCadastro]").click(function(){
             loja = localStorage.getItem("loja");
