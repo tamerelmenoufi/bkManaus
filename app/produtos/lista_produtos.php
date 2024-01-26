d50c7e6e32fa56559265082a95e8ff9418e9d17f
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-27 01:02:09 +0200

    atualizações

--- app/produtos/lista_produtos.php
+++ app/produtos/lista_produtos.php
@@ -13,7 +13,7 @@
     $lpr = mysqli_query($con, $lp);
     $combos = [];
     while($lpd = mysqli_fetch_object($lpr)){
-        $lc = "select codigo from produtos where categoria = '8' and deletado != '1' and situacao = '1' and '\"{$lpd->codigo}\"' like produtos->>'$[*].produto'";
+        $lc = "select codigo from produtos where categoria = '8' and deletado != '1' and situacao = '1' and  produtos->>'$[*].produto' like '%\"{$lpd->codigo}\"%'";
         $lcr = mysqli_query($con, $lc);
         while($lcd = mysqli_fetch_object($lcr)){
             $combos[] = $lcd->codigo;
@@ -116,9 +116,9 @@
 
 print_r($combos);
 
-echo $lp;
-echo "<br>";
-echo $lc;
+// echo $lp;
+// echo "<br>";
+// echo $lc;
 
 $query = "select * from produtos where categoria = '{$c->codigo}' and deletado != '1' and situacao = '1' order by promocao desc";
 $result = mysqli_query($con, $query);
