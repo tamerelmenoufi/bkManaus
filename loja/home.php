310b7176f4bf24f59bcd369d5ce465b8968583c1
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-27 14:48:45 +0200

    atualizações

--- loja/home.php
+++ loja/home.php
@@ -149,8 +149,8 @@
 <div class="d-flex justify-content-between legenda">
     <div class="d-flex flex-column justify-content-between w-50">
         <div class="d-flex justify-content-between" style="font-size:12px">
-            <div class="p-1"><object style="width:10px; height:10px; border-radius:2px;" class="bg-secondary"></object> Pendente</div>
-            <div class="p-1"><object style="width:10px; height:10px; border-radius:2px;" class="bg-warning"></object> Produção</div>
+            <div class="ml-3 p-1"><object style="width:10px; height:10px; border-radius:2px;" class="bg-secondary"></object> Pendente</div>
+            <div class="ml-3 p-1"><object style="width:10px; height:10px; border-radius:2px;" class="bg-warning"></object> Produção</div>
         </div>
         <div class="d-flex justify-content-between" style="font-size:12px">
             <div class="p-1"><object style="width:10px; height:10px; border-radius:2px;" class="bg-info"></object> Entrega</div>
