d84baab68309fc0f71cc32f1883a6f5f37b8450f
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-04 20:25:56 +0200

    atualizações

--- app/lib/idUnico.php
+++ app/lib/idUnico.php
@@ -3,8 +3,10 @@
 
     if($_POST['idUnico']){
         $n = mysqli_num_rows(mysqli_query($con,"select * from vendas_tmp where id_unico = '{$_POST['idUnico']}'"));
+        file_put_contents('id_unico'.$_POST['idUnico'].'.txt', "select * from vendas_tmp where id_unico = '{$_POST['idUnico']}'");
         if(!$n){
             mysqli_query($con, "insert into vendas_tmp set id_unico = '{$_POST['idUnico']}', detalhes='{}'");
+            file_put_contents('id_unico_novo-'.$_POST['idUnico'].'.txt', "select * from vendas_tmp where id_unico = '{$_POST['idUnico']}'");
         }
     }
 
