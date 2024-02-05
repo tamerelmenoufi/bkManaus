70eb26c565987d82aed79999dc86e6b37666f235
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-05 15:17:26 +0200

    atualizações

--- app/pagamento/pix.php
+++ app/pagamento/pix.php
@@ -170,10 +170,10 @@
 
                     if($operadora_id){
 
-                        $mensagem = "*BK Manaus Informa* - Sua solicitação de pagamento para o pedido *#{$pedido}* com PIX foi registrada. Aguardando confirmação.";
+                        $mensagem = "*BK Manaus Informa* - Sua solicitação de pagamento para o pedido *#{$pedido}* com PIX foi registrada. Aguardando confirmação.".date("d/m/Y H:i:s");
                         EnviarWapp($v->Ctelefone,$mensagem);
 
-                        $mensagem = "Copie o código da chave PIX para o seu pagamento diretamente no aplicativo bkManaus pelo linque {$urlApp}";
+                        $mensagem = "Copie o código da chave PIX para o seu pagamento diretamente no aplicativo bkManaus pelo linque {$urlApp}".date("d/m/Y H:i:s");
                         EnviarWapp($v->Ctelefone,$mensagem);
 
                         //$mensagem = "Para sua comodidade, enviaremos o código da chave PIX para o seu pagamento. Copie a chave e utilize o por pix (chave aleatória) no seu banco.";
