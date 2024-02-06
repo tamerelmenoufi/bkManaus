3134c5a42db21f72619147aac479b1557f89f3cf
tamerelmenoufi <tamer@mohatron.com.br>
2024-02-06 12:51:21 -0400

    atualizações

--- lista.php
+++ lista.php
@@ -18,6 +18,15 @@
         <div class="row">
             <div class="col">
                 <h4>Lista de produtos</h4>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Categoria</th>
+                            <th>Produto</th>
+                            <th>Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                 <?php
                     $query = "select * from categorias where situacao = '1' and deletado != '1' order by ordem";
                     $result = mysqli_query($con, $query);
@@ -25,10 +34,18 @@
                         $query1 = "select * from produtos where categoria = '{$c->codigo}' and situacao = '1' and deletado != '1' order by produto";
                         $result1 = mysqli_query($con, $query1);
                         while($p = mysqli_fetch_object($result1)){
-                            echo $p->produto."<br>";
+                ?>
+                        <tr>
+                            <td><?=$c->categoria?></td>
+                            <td><?=$p->produto?></td>
+                            <td><?=(($c->codigo == 8)?number_format(CalculaValorCombo($d->codigo),2,",",false):number_format($c->valor,2,",",false))?></td>
+                        </tr>
+                <?php
                         }
                     }
                 ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
