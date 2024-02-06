e9f5345dd6014e4bedb896ed31ea37569b1eee4a
tamerelmenoufi <tamer@mohatron.com.br>
2024-02-06 10:22:53 -0400

    atualizações

---
+++ lista.php
@@ -0,0 +1,38 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="img/icone.png">
+    <title>BK - Manaus</title>
+    <?php
+    include("lib/header.php");
+    ?>
+  </head>
+  <body translate="no">
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h4>Lista de produtos</h4>
+            </div>
+        </div>
+    </div>
+
+    <?php
+        include("lib/footer.php");
+    ?>
+
+    <script>
+        $(function(){
+ 
+        })
+
+    </script>
+
+  </body>
+</html>
