2845f6bc51435e6ee0a306f10a84df2d5716eec4
tamerelmenoufi <tamer@mohatron.com.br>
2024-02-06 12:36:34 -0400

    atualizações

--- lista.php
+++ lista.php
@@ -1,6 +1,5 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,10 +14,21 @@
   </head>
   <body translate="no">
 
-    <div class="container">
+    <div class="container mt-3">
         <div class="row">
             <div class="col">
                 <h4>Lista de produtos</h4>
+                <?php
+                    $query = "select * from categorias where situacao = '1' and deletado != '1' order by ordem";
+                    $result = mysqli_query($con, $query);
+                    while($c = mysqli_fetch_object($result)){
+                        $query1 = "select * from produtos where categoria = '{$c->codigo}' and situacao = '1' and deletado != '1' order by produto";
+                        $result1 = mysqli_query($con, $query1);
+                        while($p = mysqli_fetch_object($result1)){
+                            echo $d->produto."<br>";
+                        }
+                    }
+                ?>
             </div>
         </div>
     </div>
