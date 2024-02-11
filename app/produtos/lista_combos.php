926103f2b0f71c504a229b717112bb11ae70de87
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-11 09:10:04 +0200

    Atualizações

--- app/produtos/lista_combos.php
+++ app/produtos/lista_combos.php
@@ -120,7 +120,7 @@ while($d = mysqli_fetch_object($result)){
             $prd[] = $d1->produto;
         }
 
-        $prd = implode("</div><div style='color:".(($d->promocao == '1')?'#ffffff':'#000000')."';>- ", $prd);
+        $prd = implode("</div><div class='produto_dados' style='color:".(($d->promocao == '1')?'#ffffff':'#000000')."';>- ", $prd);
     }
 ?>
     <div class="produto_painel" codigo = "<?=$d->codigo?>" style="background-color:<?=(($d->promocao == '1')?'#bd0100':'trasparent')?>">
