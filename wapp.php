822bd638f83dafbc47e2782b040228f93ed5d446
tamerelmenoufi <tamer@mohatron.com.br>
2024-02-13 10:11:39 -0400

    atualizações

--- wapp.php
+++ wapp.php
@@ -1,8 +1,8 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
-
+    $con2 = AppConnect("bk");
     $query = "select * from clientes where telefone != '' and telefone_confirmado = '1'";
-    $result = mysqli_query($con, $query);
+    $result = mysqli_query($con2, $query);
     while($d = mysqli_fetch_object($result)){
 
         echo "{$d->telefone} >> {$d->nome}<br>";
