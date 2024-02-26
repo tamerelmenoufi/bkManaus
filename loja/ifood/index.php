703419b07c1e3007f01287974a9a73e0e7b6511a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-26 21:59:00 +0200

    Atualizações

--- loja/ifood/index.php
+++ loja/ifood/index.php
@@ -3,3 +3,34 @@
 ?>
 
 <h4>Pedido do ifood</h4>
+
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>Categoria</th>
+            <th>Produto</th>
+            <th>Valor</th>
+            <th>Valor no Combo</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php
+    $query = "select * from categorias where situacao = '1' and deletado != '1' order by ordem";
+    $result = mysqli_query($con, $query);
+    while($c = mysqli_fetch_object($result)){
+        $query1 = "select * from produtos where categoria = '{$c->codigo}' and situacao = '1' and deletado != '1' order by produto";
+        $result1 = mysqli_query($con, $query1);
+        while($p = mysqli_fetch_object($result1)){
+?>
+        <tr>
+            <td><?=$c->categoria?></td>
+            <td><?=$p->produto?></td>
+            <td>R$ <?=(($c->codigo == 8)?number_format(CalculaValorCombo($p->codigo),2,",",false):number_format($p->valor,2,",",false))?></td>
+            <td><?=(($c->codigo == 8)?'-':'R$ '.number_format($p->valor_combo,2,",",false))?></td>
+        </tr>
+<?php
+        }
+    }
+?>
+    </tbody>
+</table>
