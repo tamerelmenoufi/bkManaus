43e92050ab33b335c44c08433de169f46406e34d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-16 23:56:36 +0200

    Atualizações

--- src/combos/form.php
+++ src/combos/form.php
@@ -95,7 +95,7 @@
 
         $retorno = [
             'status' => true,
-            'codigo' => $cod
+            'codigo' => $query
         ];
 
         echo json_encode($retorno);
@@ -450,7 +450,7 @@
                     mimeType: 'multipart/form-data',
                     data: campos,
                     success:function(dados){
-                        // console.log(dados);
+                        console.log(dados);
                         // if(dados.status){
                             $.ajax({
                                 url:"src/combos/index.php",
