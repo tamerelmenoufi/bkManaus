d1b7d5d1fa10adacad1028ba649e26ac8849087b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-10 21:45:45 +0300

    Atualizações

---
+++ api/cliente.php
@@ -0,0 +1,30 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+    $_POST['cliente'] = 1;
+
+    $query = "select * from enderecos where cliente = '{$_POST['cliente']}'";
+    $result = mysqli_query($con, $query);
+    $enderecos = [];
+    $padrao = [];
+    while($d = mysqli_fetch_object($result)){
+        if($d->padrao){
+            $padrao = $d;
+        }
+            $enderecos[] = $d;
+    }
+
+    $query = "select * from clientes where codigo = '{$_POST['cliente']}'";
+    $result = mysqli_query($con, $query);
+    $cliente = [];
+    $d = mysqli_fetch_object($result);
+    if($d->codigo){
+        $cliente = $d;
+    }
+
+    $c = $cliente + $enderecos + $padrao;
+
+    echo json_encode($c);
