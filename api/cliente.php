9f72d86392153871b6f98d1cbe55aa8c48bb8be6
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-11 15:10:42 +0300

    Atualizações

--- api/cliente.php
+++ api/cliente.php
@@ -4,11 +4,29 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
     $_POST = json_decode(file_get_contents('php://input'), true);
 
-    $_POST['cliente'] = 1;
-
-    $query = "select * from enderecos where cliente = '{$_POST['cliente']}'";
+    $query = "select * from vendas_tmp where id_unico = '{$_POST['id_unico']}'";
     $result = mysqli_query($con, $query);
-    $c = [];
+    $d = mysqli_fetch_object($result);
+
+    if(!$d->codigo){
+
+        $query = "insert into vendas_tmp set id_unico = '{$_POST['id_unico']}', detalhes = '{}'";
+        $result = mysqli_query($query);
+        $v = mysqli_fetch_object($result);
+
+        $d->cliente = false;
+
+        $c['vendas_tmp'] = [];
+
+    }else{
+        $c['vendas_tmp'] = json_decode($d->detalhes);
+    }
+
+    $query = "select * from enderecos where cliente = '{$d->cliente}'";
+    $result = mysqli_query($con, $query);
+    $c['padrao'] = [];
+    $c['enderecos'] = [];
+    $c['cliente'] = [];
     while($d = mysqli_fetch_object($result)){
         if($d->padrao){
             $c['padrao'] = $d;
@@ -16,7 +34,7 @@
             $c['enderecos'][] = $d;
     }
 
-    $query = "select * from clientes where codigo = '{$_POST['cliente']}'";
+    $query = "select * from clientes where codigo = '{$d->cliente}'";
     $result = mysqli_query($con, $query);
     $d = mysqli_fetch_object($result);
     if($d->codigo){
