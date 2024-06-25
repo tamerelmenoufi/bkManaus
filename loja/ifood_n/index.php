feddf721922e8fdef90bc9f1d1e36c9dd9debc92
tamerelmenoufi <tamer@mohatron.com.br>
2024-06-25 09:34:07 -0400

    Atualizações

---
+++ loja/ifood_n/index.php
@@ -0,0 +1,23 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+?>
+<style>
+    
+</style>
+<h4>Pedido do ifood</h4>
+<div style="position:absolute; left:0; right:0; top:70px; bottom:0; overflow:auto; padding:10px;">
+    <div class="p-2">
+        <div class="mb-3">
+            <label for="telefone" class="form-label">Número do Pedido ifood*</label>
+            <input type="text" class="form-control" id="codigo" >
+        </div>   
+    </div>
+</div>
+
+
+
+<script>
+    $(function(){
+
+    })
+</script>
