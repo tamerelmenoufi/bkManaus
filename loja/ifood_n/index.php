0544453cc61ab396fd2c55ddcb0081973b717f49
tamerelmenoufi <tamer@mohatron.com.br>
2024-06-25 09:55:27 -0400

    Atualizações

--- loja/ifood_n/index.php
+++ loja/ifood_n/index.php
@@ -12,9 +12,37 @@
             <input type="text" class="form-control" id="codigo" >
         </div>   
     </div>
+    <div class="p-2">
+        <div class="mb-3">
+            <label for="telefone" class="form-label">Entregador*</label>
+            <select name="entregador" id="entregador">
+                <?php
+                $q = "select * from entregadores where situacao = '1' and deletado != '1' order by nome";
+                $r = mysqli_query($con, $q);
+                while($s = mysqli_fetch_object($r)){
+                ?>
+                <option value="<?=$s->codigo?>"><?=$s->nome?></option>                
+                <?php
+                }
+                ?>
+            </select>
+        </div>
+    </div>
+    <div class="p-2">
+        <div class="mb-3">
+            <label for="telefone" class="form-label">Situação*</label>
+            <select name="situacao" id="situacao">
+                <option value="0">Pendente</option>
+                <option value="1">Entregue</option>
+            </select>
+        </div>
+    </div>
+    <div class="p-2">
+        <div class="mb-3">
+            <button class="btn btn-success w-100">SALVAR DADOS</button>
+        </div>
+    </div>
 </div>
-
-
 
 <script>
     $(function(){
