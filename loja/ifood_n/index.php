c2ea2c03810de6a29206ebbb040cd801040e6db8
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-06-26 15:57:44 +0300

    Atualizações

--- loja/ifood_n/index.php
+++ loja/ifood_n/index.php
@@ -2,9 +2,24 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 
     if($_POST['acao'] == 'salvar'){
-        $query = "insert into ifood set ifood = '{$_POST['ifood']}', data = NOW(), entregador = '{$_POST['entregador']}', situacao = '{$_POST['situacao']}'";
-        mysqli_query($con, $query);
+        if($_POST['codigo']){
+            $query = "update ifood set ifood = '{$_POST['ifood']}', data = NOW(), entregador = '{$_POST['entregador']}', situacao = '{$_POST['situacao']}' where codigo = '{$_POST['codigo']}'";
+            mysqli_query($con, $query);
+        }else{
+            $query = "insert into ifood set ifood = '{$_POST['ifood']}', data = NOW(), entregador = '{$_POST['entregador']}', situacao = '{$_POST['situacao']}'";
+            mysqli_query($con, $query);            
+        }
+
     }
+
+
+    if($_POST['codigo']){
+        $query = "select * from ifood where codigo = '{$_POST['codigo']}'";
+        $result = mysqli_query($con, $query);
+        $d = mysqli_fetch_object($result);
+    }
+
+
 
 ?>
 <style>
@@ -15,7 +30,7 @@
     <div class="p-2">
         <div class="mb-3">
             <label for="telefone" class="form-label">Número do Pedido ifood*</label>
-            <input type="text" class="form-control" id="ifood" >
+            <input type="text" class="form-control" id="ifood" value="<?=$d->ifood?>" >
         </div>   
     </div>
     <div class="p-2">
@@ -27,7 +42,7 @@
                 $r = mysqli_query($con, $q);
                 while($s = mysqli_fetch_object($r)){
                 ?>
-                <option value="<?=$s->codigo?>"><?=$s->nome?></option>                
+                <option value="<?=$s->codigo?>" <?=(($s->codigo == $d->entregador)?'selected':false)?>><?=$s->nome?></option>                
                 <?php
                 }
                 ?>
@@ -38,14 +53,15 @@
         <div class="mb-3">
             <label for="telefone" class="form-label">Situação*</label>
             <select name="situacao" id="situacao" class="form-select">
-                <option value="0">Pendente</option>
-                <option value="1">Entregue</option>
+                <option value="0" <?=(($d->situacao == '0')?'selected':false)?>>Pendente</option>
+                <option value="1" <?=(($d->situacao == '1')?'selected':false)?>>Entregue</option>
             </select>
         </div>
     </div>
     <div class="p-2">
         <div class="mb-3">
             <button class="btn btn-success w-100 salvar">SALVAR DADOS</button>
+            <input type="hidden" id="codigo" value="<?=$d->codigo?>">
         </div>
     </div>
 </div>
@@ -54,6 +70,7 @@
     $(function(){
         $(".salvar").click(function(){
 
+            codigo = $("#codigo").val();
             ifood = $("#ifood").val();
             entregador = $("#entregador").val();
             situacao = $("#situacao").val();
@@ -72,6 +89,7 @@
                 url:"ifood_n/index.php",
                 type:"POST",
                 data:{
+                    codigo,
                     ifood,
                     entregador,
                     situacao,
