7e781b5f369a482aa47323efb3b13d2b4edea098
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-01 22:29:34 +0300

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -6,7 +6,7 @@
     echo $query = " SELECT
             (select count(*) from produtos where situacao = '1' and deletado != '1') as quantidade_produtos,
             (select count(*) from vendas where situacao = 'pago') as quantidade_vendas,
-            (select * from entregadores where deletado != '1') as quantidade_entregadores,
+            (select count(*) from entregadores where deletado != '1') as quantidade_entregadores,
             (select count(*) from vendas where situacao = 'pago' and producao = 'entregue') as quantidade_entregue
 
     ";
