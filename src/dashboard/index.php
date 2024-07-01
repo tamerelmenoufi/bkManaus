7c2a386e0d5b07dae501b55bd892b5b76e5b5bf1
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-01 20:12:36 +0300

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -13,9 +13,43 @@
 <style>
 
 </style>
-<div class="row mb-3 mt-3">
-    <div class="col-md-12">
-        DashBoard
+
+
+<div class="m-3">
+    
+    <div class="row g-0">
+        <div class="col-md-12 p-2">
+            <h6>Resumo Geral</h6>
+        </div>
+
+        <div class="col-md-3 p-2">
+            <div class="alert alert-danger" role="alert">
+                <span>Produtos</span>
+                <h1><?=number_format($v->pagamento_devolucao,0,',','.')?></h1>
+            </div>
+        </div>
+
+        <div class="col-md-3 p-2">
+            <div class="alert alert-success" role="alert">
+                <span>Vendas</span>
+                <h1><?=number_format($v->pagamento_produto,0,',','.')?></h1>
+            </div>
+        </div>
+
+        <div class="col-md-3 p-2">
+            <div class="alert alert-success" role="alert">
+                <span>Entregadores</span>
+                <h1><?=number_format($v->pagamento_produto,0,',','.')?></h1>
+            </div>
+        </div>
+
+        <div class="col-md-3 p-2">
+            <div class="alert alert-success" role="alert">
+                <span>Entregas</span>
+                <h1><?=number_format($v->pagamento_produto,0,',','.')?></h1>
+            </div>
+        </div>
+        
     </div>
 </div>
 
@@ -24,43 +58,36 @@
     
     <div class="row g-0">
         <div class="col-md-12 p-2">
-            <h6>Resumo <?=(($_SESSION['dashboardDataInicial'] and $_SESSION['dashboardDataFinal'])? "de ".dataBr($_SESSION['dashboardDataInicial'])." a ".dataBr($_SESSION['dashboardDataFinal']):'Geral')?></h6>
-        </div>
-        <div class="col-md-2 p-2">
-            <div class="alert alert-secondary" role="alert">
-                <span>Planilhas Importadas</span>
-                <h1><?=$v->planilhas?></h1>
-            </div>
-        </div>
-        <div class="col-md-2 p-2">
-            <div class="alert alert-primary" role="alert">
-                <span>Quantidade Vendas</span>
-                <h1><?=$v->vendas?></h1>
-            </div>
-        </div>
-
-        <div class="col-md-2 p-2">
-            <div class="alert alert-warning" role="alert">
-                <span>Quantidade Devolução</span>
-                <h1><?=$v->devolucao?></h1>
-            </div>
+            <h6>Resumo Financeiro</h6>
         </div>
 
         <div class="col-md-3 p-2">
             <div class="alert alert-danger" role="alert">
-                <span>Total Devolução</span>
+                <span>Total de Vendas</span>
                 <h1>R$ <?=number_format($v->pagamento_devolucao,2,',','.')?></h1>
             </div>
         </div>
 
-
         <div class="col-md-3 p-2">
             <div class="alert alert-success" role="alert">
-                <span>Total Vendas</span>
+                <span>Custo de entregas</span>
                 <h1>R$ <?=number_format($v->pagamento_produto,2,',','.')?></h1>
             </div>
         </div>
 
+        <div class="col-md-3 p-2">
+            <div class="alert alert-success" role="alert">
+                <span>Tickt Médio</span>
+                <h1>R$ <?=number_format($v->pagamento_produto,2,',','.')?></h1>
+            </div>
+        </div>
+
+        <div class="col-md-3 p-2">
+            <div class="alert alert-success" role="alert">
+                <span>Devoluções</span>
+                <h1>R$ <?=number_format($v->pagamento_produto,2,',','.')?></h1>
+            </div>
+        </div>
 
         
     </div>
