d799c606af3e7bd6b796dde93ab9266d02fcc25c
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-01 22:27:24 +0300

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -3,11 +3,16 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 
 
-    // echo $query = "update produtos set 
-    //                                 valor = '3.44'
-    //                         where categoria = 2
-    // ";
-    // sisLog($query);
+    $query = "
+            (select count(*) from produtos where situacao = '1' and deletado != '1') as quantidade_produtos,
+            (select count(*) from vendas where situacao = 'pago') as quantidade_vendas,
+            (select * from entregadores where deletado != '1') as quantidade_entregadores,
+            (select count(*) from vendas where situacao = 'pago' and producao = 'entregue') as quantidade_entregue
+
+    ";
+    $result = mysqli_query($con,$query);
+
+    $d = mysqli_fetch_object($result);
     
 ?>
 <style>
@@ -25,28 +30,28 @@
         <div class="col-md-3 p-2">
             <div class="alert alert-warning" role="alert">
                 <span>Produtos</span>
-                <h1><?=number_format($v->pagamento_devolucao,0,',','.')?></h1>
+                <h1><?=number_format($v->quantidade_produtos,0,',','.')?></h1>
             </div>
         </div>
 
         <div class="col-md-3 p-2">
             <div class="alert alert-success" role="alert">
                 <span>Vendas</span>
-                <h1><?=number_format($v->pagamento_produto,0,',','.')?></h1>
+                <h1><?=number_format($v->quantidade_vendas,0,',','.')?></h1>
             </div>
         </div>
 
         <div class="col-md-3 p-2">
             <div class="alert alert-primary" role="alert">
                 <span>Entregadores</span>
-                <h1><?=number_format($v->pagamento_produto,0,',','.')?></h1>
+                <h1><?=number_format($v->quantidade_entregadores,0,',','.')?></h1>
             </div>
         </div>
 
         <div class="col-md-3 p-2">
             <div class="alert alert-secondary" role="alert">
                 <span>Entregas</span>
-                <h1><?=number_format($v->pagamento_produto,0,',','.')?></h1>
+                <h1><?=number_format($v->quantidade_entregue,0,',','.')?></h1>
             </div>
         </div>
         
