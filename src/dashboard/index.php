543fafc7179bd2562bc29813044402d161135690
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-01 20:15:17 +0300

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -37,14 +37,14 @@
         </div>
 
         <div class="col-md-3 p-2">
-            <div class="alert alert-success" role="alert">
+            <div class="alert alert-primary" role="alert">
                 <span>Entregadores</span>
                 <h1><?=number_format($v->pagamento_produto,0,',','.')?></h1>
             </div>
         </div>
 
         <div class="col-md-3 p-2">
-            <div class="alert alert-success" role="alert">
+            <div class="alert alert-secondary" role="alert">
                 <span>Entregas</span>
                 <h1><?=number_format($v->pagamento_produto,0,',','.')?></h1>
             </div>
@@ -62,28 +62,28 @@
         </div>
 
         <div class="col-md-3 p-2">
-            <div class="alert alert-danger" role="alert">
+            <div class="alert alert-success" role="alert">
                 <span>Total de Vendas</span>
                 <h1>R$ <?=number_format($v->pagamento_devolucao,2,',','.')?></h1>
             </div>
         </div>
 
         <div class="col-md-3 p-2">
-            <div class="alert alert-success" role="alert">
+            <div class="alert alert-warning" role="alert">
                 <span>Custo de entregas</span>
                 <h1>R$ <?=number_format($v->pagamento_produto,2,',','.')?></h1>
             </div>
         </div>
 
         <div class="col-md-3 p-2">
-            <div class="alert alert-success" role="alert">
+            <div class="alert alert-primary" role="alert">
                 <span>Tickt Médio</span>
                 <h1>R$ <?=number_format($v->pagamento_produto,2,',','.')?></h1>
             </div>
         </div>
 
         <div class="col-md-3 p-2">
-            <div class="alert alert-success" role="alert">
+            <div class="alert alert-danger" role="alert">
                 <span>Devoluções</span>
                 <h1>R$ <?=number_format($v->pagamento_produto,2,',','.')?></h1>
             </div>
