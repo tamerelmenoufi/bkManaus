1003a3850dd8b9f41e97f88a173d9af9a0166491
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-02 21:08:42 +0300

    Atualizações

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -75,7 +75,14 @@
     <div class="row mb-1">
       <div class="col">
         <a url="src/relatorios/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
-          <i class="fa-regular fa-user col-1"></i> <span class="col-11">Vendas</span>
+          <i class="fa-regular fa-user col-1"></i> <span class="col-11">Vendas - BkManaus</span>
+        </a>
+      </div>
+    </div>  
+    <div class="row mb-1">
+      <div class="col">
+        <a url="src/ifood/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-regular fa-user col-1"></i> <span class="col-11">Vendas - Ifood</span>
         </a>
       </div>
     </div>  
---
+++ src/ifood/index.php
@@ -0,0 +1,52 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+
+?>
+
+<!-- <div class="row g-0">
+    <div class="col-12">
+        <div class="m-3">
+            <h3>Calendário de Produção</h3>
+            <div class="d-flex justify-content-between">
+                <?php
+                    $y = ($_SESSION['buscaY'])?:date("Y");
+                    $di = mktime(0,0,0, date("m"), 1, $y);
+                    $df = mktime(0,0,0, date("m")+1, 1 - 1, $y);
+                    echo "dia 1 :".date("d/m/Y", $di);
+                    echo " e dia 2 :".date("d/m/Y", $df);
+                ?>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+<div class="row g-0">
+    <h3 class="m-3">Calendário de Produção</h3>
+    <div class="col-4">
+        <div class="area_calendario m-3"></div>
+    </div>
+    <div class="col-8">
+        <div class="dados_calncario m-3"></div>
+    </div>
+    
+</div>
+
+<script>
+    $(function(){
+
+        Carregando('none')
+
+        $.ajax({
+            url:"src/ifood/calendario.php",
+            type:"POST",
+            data:{
+
+            },
+            success:function(dados){
+                $(".area_calendario").html(dados);
+            }
+        })
+
+    })
+</script>
