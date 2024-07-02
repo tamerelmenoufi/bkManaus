b740d890fb6710b31ace7b93620f79c7676d747d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-03 01:56:35 +0300

    Atualizações

--- src/ifood/entregadores.php
+++ src/ifood/entregadores.php
@@ -4,6 +4,33 @@
 ?>
 
 <h3>Produção por Entregadores</h3>
+
+<div class="row">
+    <div class="col-md-3">
+        <input type="data" id="data_inicio" value="<?=$_POST['data_inicio']?>">
+    </div>
+    <div class="col-md-3">
+        <input type="data" id="data_fim" value="<?=$_POST['data_fim']?>">
+    </div>
+    <div class="col-md-3">
+        <select id="entregadores">
+            <option value="todos">Todos</option>
+            <?php
+            $q = "select * from entregadores where situacao = '1' and deletado != '1' order by nome asc";
+            $r = mysqli_query($con, $q);
+            while($s = mysqli_fetch_object($result)){
+            ?>
+            <option value="<?=$s->codigo?>"><?=$s->nome?></option>
+            <?php
+            }
+            ?>
+        </select>
+    </div>
+    <div class="col-md-3">
+        <button class="btn btn-primary w-100 lista_entregadores">Filtrar Dados</button>
+    </div>
+</div>
+
 <table class="table table-hover">
     <thead>
         <tr>
@@ -16,7 +43,7 @@
     </thead>
     <tbody>
         <?php
-        $query = "select a.*, b.nome as entregador from ifood a left join entregadores b on a.entregador = b.codigo";
+        $query = "select a.*, b.nome as entregador from ifood a left join entregadores b on a.entregador = b.codigo order by b.nome asc, a.data asc";
         $result = mysqli_query($con, $query);
         while($d = mysqli_fetch_object($result)){
         ?>
