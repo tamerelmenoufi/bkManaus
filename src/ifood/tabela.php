d2c8d1f85e5b57909fda7e685dadef38ed0138e2
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-03 01:08:50 +0300

    Atualizações

--- src/ifood/tabela.php
+++ src/ifood/tabela.php
@@ -4,31 +4,33 @@
 ?>
 
 <h3>Lista do período de <?=$_POST['data']?></h3>
-<table class="table table-hover">
-    <thead>
-        <tr>
-            <th>Data</th>
-            <th>Pedido</th>
-            <th>Valor</th>
-            <th>Entregador</th>
-            <th>Situação</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        $query = "select * from ifood where data like '%{$_POST['data']}%'";
-        $result = mysqli_query($con, $query);
-        while($d = mysqli_fetch_object($result)){
-        ?>
-        <tr>
-            <td><?=$d->data?></td>
-            <td><?=$d->ifood?></td>
-            <td><?=$d->valor?></td>
-            <td><?=$d->entregador?></td>
-            <td><?=$d->producao?></td>
-        </tr>
-        <?php
-        }
-        ?>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Data</th>
+                <th>Pedido</th>
+                <th>Valor</th>
+                <th>Entregador</th>
+                <th>Situação</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $query = "select * from ifood where data like '%{$_POST['data']}%'";
+            $result = mysqli_query($con, $query);
+            while($d = mysqli_fetch_object($result)){
+            ?>
+            <tr>
+                <td><?=$d->data?></td>
+                <td><?=$d->ifood?></td>
+                <td><?=$d->valor?></td>
+                <td><?=$d->entregador?></td>
+                <td><?=$d->producao?></td>
+            </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
