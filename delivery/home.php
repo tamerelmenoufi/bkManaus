a1175e6fd674f155e0d336f78d27570f0bc9f107
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-03 04:20:49 +0300

    Atualizações

--- delivery/home.php
+++ delivery/home.php
@@ -82,7 +82,7 @@
 
         <ul class="list-group">
             <?php
-            echo $query = "(select 
+            $query = "(select 
                             'pedido' as tipo,
                             a.*, 
                             if(a.producao = 'pendente',0,1) as ordem, 
