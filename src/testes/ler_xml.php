9f9b906341474fc73efff746c76d5f78b7fc462d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-04 16:18:50 +0300

    Atualizações

--- src/testes/ler_xml.php
+++ src/testes/ler_xml.php
@@ -3,9 +3,13 @@
 
     $xml = simplexml_load_file('teste'.$_GET['n'].'.xml') or die("Erro: Não foi possível carregar o arquivo XML.");
 
+    $json = json_encode($xml);
+
+
+
 
     echo "<pre>";
 
-    print_r($xml);
+    print_r($json);
 
     echo "</pre>";
