f2fe9ee4d760b2a7bb0ef6c0204a9b4def2897fc
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-04 12:03:20 -0400

    Atualizações

--- src/estoque/saida/index.php
+++ src/estoque/saida/index.php
@@ -3,3 +3,12 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 
     echo $_SERVER['PHP_SELF'];
+
+    ?>
+
+    <script>
+        $(function(){
+            Carregando('none');
+            
+        })
+    </script>
