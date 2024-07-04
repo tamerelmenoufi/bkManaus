0701856aa4b2a692f0f527f55e39ada9cb5e533d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-05 00:53:05 +0300

    Atualizações

--- src/estoque/entrada/produtos.php
+++ src/estoque/entrada/produtos.php
@@ -1,9 +1,9 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 
-    if($_POST['nota']) $_SESSION['notas'] = $_POST['nota'];
+    if($_POST['nota']) $_SESSION['nota'] = $_POST['nota'];
 
-   echo  $query = "select * from notas where codigo = '{$_SESSION['nota']}'";
+    $query = "select * from notas where codigo = '{$_SESSION['nota']}'";
     $result = mysqli_query($con, $query);
     $d = mysqli_fetch_object($result);
 
