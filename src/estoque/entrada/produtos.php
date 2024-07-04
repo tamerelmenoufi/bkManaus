1c8966a613b68b585b39e22f710f62432f6c91d0
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-05 01:45:09 +0300

    Atualizações

--- src/estoque/entrada/produtos.php
+++ src/estoque/entrada/produtos.php
@@ -12,68 +12,75 @@
 
     function produtoMapa($p){
 ?>
-    <tr>
-        <td>Código do produto</td>
-        <td><?=$p->cProd?></td>
-    </tr>
-    <tr>
-        <td>Código de barras do produto</td>
-        <td><?=$p->cEAN?></td>
-    </tr>
-    <tr>
-        <td>Descrição do produto</td>
-        <td><?=$p->xProd?></td>
-    </tr>
-    <tr>
-        <td>NCM</td>
-        <td><?=$p->NCM?></td>
-    </tr>
-    <tr>
-        <td>CFOP</td>
-        <td><?=$p->CFOP?></td>
-    </tr>
-    <tr>
-        <td>Unidade</td>
-        <td><?=$p->uCom?></td>
-    </tr>
-    <tr>
-        <td>Quantidade</td>
-        <td><?=$p->qCom?></td>
-    </tr>
-    <tr>
-        <td>Valor unitário</td>
-        <td><?=$p->vUnCom?></td>
-    </tr>
-    <tr>
-        <td>Valor total bruto</td>
-        <td><?=$p->vProd?></td>
-    </tr>
-    <tr>
-        <td>Código de barras tributável</td>
-        <td><?=$p->cEANTrib?></td>
-    </tr>
-    <tr>
-        <td>Unidade tributável</td>
-        <td><?=$p->uTrib?></td>
-    </tr>
-    <tr>
-        <td>Quantidade tributáve</td>
-        <td><?=$p->qTrib?></td>
-    </tr>
-    <tr>
-        <td>Valor unitário de tributação</td>
-        <td><?=$p->vUnTrib?></td>
-    </tr>
-    <tr>
-        <td>Indicador de totalização</td>
-        <td><?=(($p->indTot)?'Sim':'Não')?></td>
-    </tr>
+<div class="card">
+  <h5 class="card-header"><?=$p->xProd?></h5>
+  <div class="card-body">
+    <table class="table">
+        <tbody>
+
+            <tr>
+                <td>Código do produto</td>
+                <td><?=$p->cProd?></td>
+            </tr>
+            <tr>
+                <td>Código de barras do produto</td>
+                <td><?=$p->cEAN?></td>
+            </tr>
+            <tr>
+                <td>Descrição do produto</td>
+                <td><?=$p->xProd?></td>
+            </tr>
+            <tr>
+                <td>NCM</td>
+                <td><?=$p->NCM?></td>
+            </tr>
+            <tr>
+                <td>CFOP</td>
+                <td><?=$p->CFOP?></td>
+            </tr>
+            <tr>
+                <td>Unidade</td>
+                <td><?=$p->uCom?></td>
+            </tr>
+            <tr>
+                <td>Quantidade</td>
+                <td><?=$p->qCom?></td>
+            </tr>
+            <tr>
+                <td>Valor unitário</td>
+                <td><?=$p->vUnCom?></td>
+            </tr>
+            <tr>
+                <td>Valor total bruto</td>
+                <td><?=$p->vProd?></td>
+            </tr>
+            <tr>
+                <td>Código de barras tributável</td>
+                <td><?=$p->cEANTrib?></td>
+            </tr>
+            <tr>
+                <td>Unidade tributável</td>
+                <td><?=$p->uTrib?></td>
+            </tr>
+            <tr>
+                <td>Quantidade tributáve</td>
+                <td><?=$p->qTrib?></td>
+            </tr>
+            <tr>
+                <td>Valor unitário de tributação</td>
+                <td><?=$p->vUnTrib?></td>
+            </tr>
+            <tr>
+                <td>Indicador de totalização</td>
+                <td><?=(($p->indTot)?'Sim':'Não')?></td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+</div>    
 <?php
     }
-?>
-<table class="table">
-    <tbody>
-<?php
+
     if(is_array($n->NFe->infNFe->det)){
         foreach($n->NFe->infNFe->det as $i => $val){
             // echo $val->prod->xProd."<br>";
@@ -84,5 +91,3 @@
         produtoMapa($n->NFe->infNFe->det->prod);
     }
 ?>
-    </tbody>
-</table>
