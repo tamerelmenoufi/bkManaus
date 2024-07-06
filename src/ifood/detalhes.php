89dc87a13048241eada52c64ad86bde93848d2d9
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-06 04:24:42 +0300

    Atualizações

--- src/ifood/detalhes.php
+++ src/ifood/detalhes.php
@@ -78,7 +78,7 @@
         <button class="btn btn-primary lista_entregadores W-100">Filtrar Dados</button>
     </div>
     <div class="col-md-1 mb-2">
-        <a href="src/ifood/detalhes_print.php" target="_blank" class="btn btn-warning btn-sm ms-2"><i class="fa-solid fa-file-csv"></i></a>
+        <a href="src/ifood/detalhes_print.php" target="_blank" class="btn btn-warning btn-sm W-100"><i class="fa-solid fa-file-csv"></i></a>
     </div>
 </div>
 
