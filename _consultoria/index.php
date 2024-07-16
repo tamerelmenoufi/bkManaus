7d599ad36360c978f4637f789c0d83207abbf83b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-16 03:50:51 +0300

    Atualizações

--- _consultoria/index.php
+++ _consultoria/index.php
@@ -45,7 +45,7 @@
 
     echo $query = "
     
-        SELECT {$campos}, concat('Semana de 13/06 a 15/06') as semana FROM app_acessos a left join vendas_tmp b on a.device = b.id_unico left join clientes c on a.cliente = c.codigo where a.data BETWEEN '2024-06-13 00:00:00' and '2024-06-15 23:59:59' and a.device != '' group by a.device order by c.nome desc, b.detalhes asc  UNION
+        (SELECT {$campos}, concat('Semana de 13/06 a 15/06') as semana FROM app_acessos a left join vendas_tmp b on a.device = b.id_unico left join clientes c on a.cliente = c.codigo where a.data BETWEEN '2024-06-13 00:00:00' and '2024-06-15 23:59:59' and a.device != '' group by a.device order by c.nome desc, b.detalhes asc)  UNION
         SELECT {$campos}, concat('Semana de 20/06 a 22/06') as semana FROM app_acessos a left join vendas_tmp b on a.device = b.id_unico left join clientes c on a.cliente = c.codigo where a.data BETWEEN '2024-06-20 00:00:00' and '2024-06-22 23:59:59' and a.device != '' group by a.device UNION
         SELECT {$campos}, concat('Semana de 20/27 a 29/06') as semana FROM app_acessos a left join vendas_tmp b on a.device = b.id_unico left join clientes c on a.cliente = c.codigo where a.data BETWEEN '2024-06-27 00:00:00' and '2024-06-29 23:59:59' and a.device != '' group by a.device UNION 
         SELECT {$campos}, concat('Semana de 04/07 a 06/07') as semana FROM app_acessos a left join vendas_tmp b on a.device = b.id_unico left join clientes c on a.cliente = c.codigo where a.data BETWEEN '2024-07-04 00:00:00' and '2024-07-06 23:59:59' and a.device != '' group by a.device UNION
