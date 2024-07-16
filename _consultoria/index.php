e96f8fff9d8f3c5be09cb648f87cd85002b0fa33
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-16 03:25:53 +0300

    Atualizações

--- _consultoria/index.php
+++ _consultoria/index.php
@@ -1,6 +1,45 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+?><!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="img/icone.png">
+    <title>BK - Manaus</title>
+    <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet"
+>
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSnblPMOwEdteX5UPYXf7XUtJYcbypx6w&callback=initMap&v=weekly&language=pt&region=BR"
+></script>
 
+<link href="https://painel.bkmanaus.com.br/lib/vendor/bootstrap-5.2.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<link href="https://painel.bkmanaus.com.br/lib/vendor/jquery-confirm-v3.3.4/dist/jquery-confirm.min.css" rel="stylesheet" >
+
+
+<script src="https://painel.bkmanaus.com.br/lib/vendor/jquery-3.6.0/jquery-3.6.0.min.js" ></script>
+<script src="https://painel.bkmanaus.com.br/lib/vendor/jquery-confirm-v3.3.4/dist/jquery-confirm.min.js" ></script>
+
+<link href="https://painel.bkmanaus.com.br/lib/vendor/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
+
+<script src="https://painel.bkmanaus.com.br/lib/vendor/jQuery-Mask/jquery.mask.min.js" ></script>
+
+<script src="https://painel.bkmanaus.com.br/lib/vendor/chart/chart.min.js" ></script>
+
+<link href="https://painel.bkmanaus.com.br/lib/css/app.css" rel="stylesheet">
+<script src="https://painel.bkmanaus.com.br/lib/js/app.js?20240715202344" ></script>
+
+<link href="https://painel.bkmanaus.com.br/lib/vendor/slick/slick.css" rel="stylesheet">
+<script src="https://painel.bkmanaus.com.br/lib/vendor/slick/slick.js" ></script>
+
+
+<script src="https://painel.bkmanaus.com.br/lib/vendor/jquery-maskmoney/jquery.maskMoney.js" ></script>  </head>
+  <body translate="no">
+
+
+  <?php
     $campos = " a.device, c.nome, b.detalhes";
 
     $query = "
@@ -16,7 +55,7 @@
     $result = mysqli_query($con, $query);
 
 
-    echo "<table>";
+    echo "<table class='table table-hover'>";
     $i = 1;
     $semana = false;
     while($d = mysqli_fetch_object($result)){
@@ -44,3 +83,8 @@
     }
 
     echo "</table>";
+    ?>
+
+
+    </body>
+</html>
