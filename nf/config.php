6671a4be039b69fe1bbc41c8e9500beae8eb3e69
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-17 03:44:29 +0300

    Atualizações

--- nf/config.php
+++ nf/config.php
@@ -8,4 +8,4 @@ $PDO = new PDO("mysql:host=yobom.com.br;dbname=bk_estoque;charset=utf8", "root",
 $endpoint = "https://nf.bkmanaus.com.br/API-NFE/api-nfe/"; // COM BARRA NO FINAL
 
 
-ini_set('display_errors', 'off');
+ini_set('display_errors', 'On');
