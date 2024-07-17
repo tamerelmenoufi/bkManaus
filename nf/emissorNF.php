75d531f5433148afd9fc156c575533a19e6468b8
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-17 20:27:55 +0300

    Atualizações

--- nf/emissorNF.php
+++ nf/emissorNF.php
@@ -383,7 +383,7 @@ $_POST['e'] = true;
 
 			$data_nfe['produtos'][$x]['impostos']['icms']['codigo_cfop'] = $cfop; // CFOP do produto
 			$data_nfe['produtos'][$x]['impostos']['icms']['origem'] = $origem; // origem do produto
-			$data_nfe['produtos'][$x]['impostos']["icms"]["alicota"] = $impostos->IPI->IPINT->pICMS;
+			$data_nfe['produtos'][$x]['impostos']["icms"]["pICMS"] = $impostos->IPI->IPINT->pICMS;
 			$data_nfe['produtos'][$x]['impostos']["icms"]["modBC"] = $impostos->IPI->IPINT->modBC;
 
 			// Sempre colocar o cst (código da situação tributária) da nota original
