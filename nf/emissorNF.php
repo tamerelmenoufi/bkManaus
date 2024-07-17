6afc2541a17b9f122ba9dc6908b72fd0f082bee7
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-07-18 02:21:42 +0300

    Atualizações

--- nf/emissorNF.php
+++ nf/emissorNF.php
@@ -140,9 +140,9 @@ $_POST['e'] = true;
 
 
 		// PEDIDO / VENDA / AQUI AS INFOMACOES PRINCIPAIS
-		$id = $BlcDados->NFe;
+		print_r($BlcDados->NFe);
 		$data_nfe = array(
-			'nfe_referenciada' => $id['Id'], //'', //vazio ou a [chave] da nota para entrada
+			//'nfe_referenciada' => $id['Id'], //'', //vazio ou a [chave] da nota para entrada
 			'ID' => $rowVenda["codigo"], // ID DA VENDA NO SISTEMA
 			'NF' => $nota['numero_proxima_nfc'], // Número da NF (Deve seguir uma ordem exata)
 			'serie' => $nota['numero_proxima_nfc'],
