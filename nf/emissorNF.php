ce27e556c961ea2430d15eab645f38fa6658dee0
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-17 17:15:06 -0400

    Atualizações

--- nf/emissorNF.php
+++ nf/emissorNF.php
@@ -136,7 +136,7 @@ $_POST['e'] = true;
 
 		// PEDIDO / VENDA / AQUI AS INFOMACOES PRINCIPAIS
 		$data_nfe = array(
-			'nfe_referenciada' => $Blc['Id'], //'', //vazio ou a [chave] da nota para entrada
+			'nfe_referenciada' => $Blc->Id, //'', //vazio ou a [chave] da nota para entrada
 			'ID' => $rowVenda["codigo"], // ID DA VENDA NO SISTEMA
 			'NF' => $nota['numero_proxima_nfc'], // Número da NF (Deve seguir uma ordem exata)
 			'serie' => $nota['numero_proxima_nfc'],
