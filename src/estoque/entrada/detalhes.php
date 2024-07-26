8be29130cd96c15db78d73e47de52d30728c1092
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-26 19:40:15 -0400

    Atualizações

--- src/estoque/entrada/detalhes.php
+++ src/estoque/entrada/detalhes.php
@@ -10,7 +10,20 @@
 
  ?>
  
- <div class="card">
+ <div class="card mb-3">
+  <div class="card-header">
+    Featured
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+</div>
+
+
+ 
+<div class="card mb-3">
   <div class="card-header">
     Featured
   </div>
