d95c78175a1057a63b1112a6d984c269d1b7ff5f
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-27 14:43:31 -0400

    Atualizações

--- src/estoque/empresas/index.php
+++ src/estoque/empresas/index.php
@@ -9,7 +9,7 @@
   <div class="card-body">
     <h5 class="card-title">Consulta de Estoque por empresa</h5>
     <p class="card-text">Selecione uma empresa para verificação do estque de produtos.</p>
-    <select name="empresa" id="empresa" class="form-select">
+    <select id="empresa" class="form-select mb-2">
         <option value="">:: Selecione uma empresa ::</option>
         <?php
             $q = "select * from empresas where tipo = 'g' order by nome asc";
@@ -21,7 +21,7 @@
             }
         ?>  
     </select>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <button class="btn btn-primary" listaEstoque>Listar Produtos</button>
   </div>
 </div>
 
@@ -30,5 +30,28 @@
 <script>
   $(function(){
       Carregando('none');
+
+      $("button[listaEstoque]").click(function(){
+        empresa = $("#empresa").val();
+        if(!empresa){
+            $.alert({
+                type:"red",
+                title:"Alerta de Pendências",
+                content:"Selecione uma empresa para listar o estoque disponível"
+            })
+            return false
+        }
+        Carregando();
+        $.ajax({
+            url:"src/estoque/empresas/index.php",
+            type:"POST",
+            data:{
+                empresa
+            },
+            success:function(dados){
+                $("#paginaHome").html(dados);
+            }
+        })
+      })
   })
 </script>
