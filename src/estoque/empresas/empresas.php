e8f196f61b7def20d08d3331918f574d1246af8c
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-27 19:55:01 -0400

    Atualizações

--- src/estoque/empresas/empresas.php
+++ src/estoque/empresas/empresas.php
@@ -32,6 +32,7 @@
                 },
                 success:function(dados){
                     $("#paginaHome").html(dados);
+                    listaEmpresas.close();
                 }
             })
         })
