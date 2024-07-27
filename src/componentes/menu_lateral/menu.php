3ab682b7ffea6f18972f99d88a4492a258a3938e
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-27 14:27:50 -0400

    Atualizações

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -97,21 +97,13 @@
       </div>
     </div>
 
-    <?php
-    $q = "select * from empresas where tipo = 'g' order by nome asc";
-    $r = mysqli_query($conE, $q);
-    while($e = mysqli_fetch_object($r)){
-    ?>
     <div class="row mb-1">
       <div class="col">
-        <a url="src/estoque/empresas/index.php?cod=<?=$e->codigo?>" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
-          <i class="fa-regular fa-user col-1"></i> <span class="col-11"><?=$e->nome?></span>
+        <a url="src/estoque/empresas/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-regular fa-user col-1"></i> <span class="col-11">Estoque</span>
         </a>
       </div>
     </div>  
-    <?php
-    }
-    ?>
 
 
     <div class="row mb-1">
--- src/estoque/empresas/index.php
+++ src/estoque/empresas/index.php
@@ -1,9 +1,25 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
 
-    print_r($_GET);
-
 ?>
+
+<?php
+    $q = "select * from empresas where tipo = 'g' order by nome asc";
+    $r = mysqli_query($conE, $q);
+    while($e = mysqli_fetch_object($r)){
+    ?>
+    <div class="row mb-1">
+      <div class="col">
+        <a url="src/estoque/empresas/index.php?cod=<?=$e->codigo?>" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-regular fa-user col-1"></i> <span class="col-11"><?=$e->nome?></span>
+        </a>
+      </div>
+    </div>  
+    <?php
+    }
+    ?>
+
+
 
 <script>
   $(function(){
