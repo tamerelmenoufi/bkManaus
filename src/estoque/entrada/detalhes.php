64155832b1d8fbec3c2d0c23e88ea9797c5a32bc
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-27 10:43:11 -0400

    Atualizações

--- src/estoque/entrada/detalhes.php
+++ src/estoque/entrada/detalhes.php
@@ -11,11 +11,18 @@
     //var_dump($d);
 
     $infPrincipal = [
-        ['Número', 'nNF'],
-        ['Série', 'serie'],
-        ['Tipo', 'tpNF'],
-        ['Data da Emissão', 'dhEmi'],
-        ['Descrição', 'natOp'],
+        ['Número', $x->NFe->infNFe->ide->nNF],
+        ['Série', $x->NFe->infNFe->ide->serie],
+        ['Tipo', $x->NFe->infNFe->ide->tpNF],
+        
+        ['Emitente CNPJ', $x->NFe->infNFe->emit->CNPJ],
+        ['Emitente', $x->NFe->infNFe->emit->xNome],
+
+        ['Destinatário CNPJ', $x->NFe->infNFe->dest->CNPJ],
+        ['Destinatário', $x->NFe->infNFe->dest->xNome],
+
+        ['Data da Emissão', $x->NFe->infNFe->ide->dhEmi],
+        ['Descrição', $x->NFe->infNFe->ide->natOp],
     ];
 
  ?>
@@ -28,10 +35,11 @@
     <li class="list-group-item">
         <?php
         foreach($infPrincipal as $i => $v){
+            
         ?>
         <div class="d-flex justify-content-between">
             <span><?=$v[0]?></span>
-            <div><?=$x->NFe->infNFe->ide->$v[1]?></div>
+            <div><?=$v[1]?></div>
             <!--<button class="btn btn-danger btn-sm">OK</button>-->
         </div>
         <?php
@@ -39,7 +47,7 @@
         ?>
         <div class="d-flex justify-content-between">
             <span>Arquivo XML</span>
-            <a href='./src/volumes/notas/xml/<?=$d->xml?>' target="_blank" class="btn btn-link"><?=$d->xml?></a>
+            <a href='./src/volumes/notas/xml/<?=$d->xml?>' target="_blank" class="btn btn-link btn-sm"><?=$d->xml?></a>
             <!--<button class="btn btn-danger btn-sm">OK</button>-->
         </div>
     </li>
