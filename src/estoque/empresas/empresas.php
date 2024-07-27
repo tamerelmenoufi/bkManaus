5d7a1fc403548f6ddcf0154458647f286aa113d7
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-27 18:42:44 -0400

    Atualizações

---
+++ src/estoque/empresas/empresas.php
@@ -0,0 +1,18 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkManaus/lib/includes.php");
+
+    if($_POST['empresa']) $_SESSION['estoque']['empresa'] = $_POST['empresa'];
+?>
+
+<ul class="list-group">
+<?php
+
+    $query = "select * from empresas where tipo = 'g' order by nome asc";
+    $result = mysqli_query($con, $query);
+    while($d = mysqli_fetch_object($result)){
+?>
+  <li class="list-group-item"><?=$d->nome?></li>
+<?php
+    }
+?>
+</ul>
--- src/estoque/empresas/venda.php
+++ src/estoque/empresas/venda.php
@@ -3,9 +3,36 @@
 
     if($_POST['empresa']) $_SESSION['estoque']['empresa'] = $_POST['empresa'];
 
-    
+
 ?>
 
 <div class="d-flex justify-content-end">
     <button iniciar_venda class="btn btn-success btn-sm"><i class="fa-solid fa-bag-shopping"></i> Iniciar uma Venda</button>
 </div>
+
+
+<script>
+    $(function(){
+
+        $("button[iniciar_venda]").click(function(){
+
+            Carregando();
+            $.ajax({
+                url:"src/estoque/empresas/empresas.php",
+                type:"POST",
+                data:{
+                    empresa:'<?=$_SESSION['estoque']['empresa']?>'
+                },
+                success:function(dados){
+                    $.dialog({
+                        title:"Empresas Cadastradas",
+                        content:dados,
+                        type:"blue"
+                    })
+                }
+            })
+
+        })
+
+    })
+</script>
