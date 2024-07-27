8eaecc4a4506db3e964b8a5507f663cb748a4af5
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-27 14:30:47 -0400

    Atualizações

--- src/estoque/empresas/index.php
+++ src/estoque/empresas/index.php
@@ -3,6 +3,17 @@
 
 ?>
 
+
+<div class="card m-3">
+  <h5 class="card-header">Featured</h5>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
 <?php
     $q = "select * from empresas where tipo = 'g' order by nome asc";
     $r = mysqli_query($con, $q);
