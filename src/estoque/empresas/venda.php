04cb3862f56a86810aa5a5bc7ac38f83dcbaf409
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-31 16:31:13 -0400

    Atualizações

--- src/estoque/empresas/venda.php
+++ src/estoque/empresas/venda.php
@@ -7,7 +7,9 @@
     if($_POST['acao'] == 'devolver'){
 
         echo $query = "update estoque_{$_SESSION['estoque']['empresa']} set qCom = (qCom + (select qCom from movimentacao where codigo = '{$_POST['item']}')) where cProd = (select cProd from movimentacao where codigo = '{$_POST['item']}')";
-        mysqli_query($con, $query);
+        if(mysqli_query($con, $query)){
+            mysqli_query($con, "delete from movimentacao where codigo = '{$_POST['item']}'");
+        }
         exit();
     }
 
