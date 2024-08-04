c2cac3c3558b5b0a99ca4a1fbcff36678baee66c
tamerelmenoufi <tamer@mohatron.com.br>
2024-08-04 12:50:14 -0400

    Atualizações

--- app/usuarios/principal.php
+++ app/usuarios/principal.php
@@ -71,7 +71,8 @@ $(function(){
                             '<div class="mb-3">' +
                             '<label for="codigoValida" class="form-label">Telefone</label>' +
                             '<input type="text" inputmode="numeric" class="form-control codigoValida" id="codigoValida" aria-describedby="validarMensagem">' +
-                            '<div id="validarMensagem" class="form-text">Digite o código <b>'+dados.codigo+'</b>  no campo acima!</div>' +
+                            '<!--<div id="validarMensagem" class="form-text">Digite o código <b>'+dados.codigo+'</b>  no campo acima!</div>-->' +
+                            '<div class="alert alert-primary" role="alert"><h1 class="text-center">'+dados.codigo+'</h1><p class="text-center">Digite o código no campo acima!</p></div>' +
                             '</div>' +
                             '</form>',
                             buttons: {
