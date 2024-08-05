cc1fdd768f6f90d89d14df6b0baa135a2ba994fa
tamerelmenoufi <tamer@mohatron.com.br>
2024-08-04 20:04:48 -0400

    Atualizações

--- app/pedido/pedidos_historico.php
+++ app/pedido/pedidos_historico.php
@@ -26,7 +26,7 @@
 
 <?php
 
-    $query = "select *, pix_detalhes->>'$.id' as operadora_id from vendas where (device = '{$_SESSION['idUnico']}' or cliente = '{$_SESSION['codUsr']}') and situacao = 'pendente'";
+    $query = "select *, pix_detalhes->>'$.id' as operadora_id from vendas where (/*device = '{$_SESSION['idUnico']}' or */cliente = '{$_SESSION['codUsr']}') and situacao = 'pendente'";
     $result = mysqli_query($con, $query);
 
     $q = mysqli_num_rows($result);
@@ -95,7 +95,7 @@
 
 if(!$_POST['novoPedido']){
     
-    $query = "select * from vendas where (device = '{$_SESSION['idUnico']}' or cliente = '{$_SESSION['codUsr']}') and situacao != 'pendente' order by codigo desc ";
+    $query = "select * from vendas where (/*device = '{$_SESSION['idUnico']}' or */cliente = '{$_SESSION['codUsr']}') and situacao != 'pendente' order by codigo desc ";
     $result = mysqli_query($con, $query);
 
     $q = mysqli_num_rows($result);
